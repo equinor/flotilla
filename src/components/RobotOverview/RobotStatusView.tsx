e9c97804a0564aff145200b159cefa276bb43446
Vetle Koppergård <vetk@equinor.com>
2021-12-14 12:19:52 +0100

    Add RobotStatusView component

---
+++ src/components/RobotOverview/RobotStatusView.tsx
@@ -0,0 +1,25 @@
+import { Typography } from '@equinor/eds-core-react'
+import { RobotStatus } from 'models/robot'
+import styles from './robotOverview.module.css'
+
+export interface RobotStatusViewProps {
+    status: RobotStatus
+}
+
+const RobotStatusView = ({ status }: RobotStatusViewProps): JSX.Element => {
+    let style_background
+    if (status === RobotStatus.Available) {
+        style_background = styles.available
+    } else if (status === RobotStatus.MissionInProgress) {
+        style_background = styles.missionInProgress
+    } else {
+        style_background = styles.offline
+    }
+    return (
+        <div className={style_background}>
+            <Typography style={{ textAlign: 'center' }}>{status}</Typography>
+        </div>
+    )
+}
+
+export default RobotStatusView
