d69eee80967bb920b3e3e2fe0db7155d114e957a
Vetle Koppergård <vetk@equinor.com>
2021-12-14 15:59:33 +0100

    Add settings for prettier

--- src/components/InfoButton/InfoButton.tsx
+++ src/components/InfoButton/InfoButton.tsx
@@ -1,38 +1,38 @@
-import { info_circle } from "@equinor/eds-icons";
-import { Button, Icon } from "@equinor/eds-core-react";
-import { Popover } from "@equinor/eds-core-react";
-import { useRef, useState } from "react";
+import { info_circle } from '@equinor/eds-icons'
+import { Button, Icon } from '@equinor/eds-core-react'
+import { Popover } from '@equinor/eds-core-react'
+import { useRef, useState } from 'react'
 
-Icon.add({ info_circle });
+Icon.add({ info_circle })
 
 interface InfoButtonProps {
-  title: string;
-  content: JSX.Element;
+    title: string
+    content: JSX.Element
 }
 
 const InfoButton = ({ title, content }: InfoButtonProps): JSX.Element => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const anchorRef = useRef<HTMLButtonElement>(null);
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const anchorRef = useRef<HTMLButtonElement>(null)
 
-  const closePopover = () => setIsOpen(false);
-  const toggleOpen = () => setIsOpen(!isOpen);
-  return (
-    <>
-      <Button ref={anchorRef} variant="ghost_icon" onClick={toggleOpen}>
-        <Icon name="info_circle" size={24} color="primary" />
-      </Button>
-      <Popover
-        id="click-popover"
-        aria-expanded={isOpen}
-        anchorEl={anchorRef.current}
-        onClose={closePopover}
-        open={isOpen}
-      >
-        <Popover.Title>{title}</Popover.Title>
-        <Popover.Content>{content}</Popover.Content>
-      </Popover>
-    </>
-  );
-};
+    const closePopover = () => setIsOpen(false)
+    const toggleOpen = () => setIsOpen(!isOpen)
+    return (
+        <>
+            <Button ref={anchorRef} variant="ghost_icon" onClick={toggleOpen}>
+                <Icon name="info_circle" size={24} color="primary" />
+            </Button>
+            <Popover
+                id="click-popover"
+                aria-expanded={isOpen}
+                anchorEl={anchorRef.current}
+                onClose={closePopover}
+                open={isOpen}
+            >
+                <Popover.Title>{title}</Popover.Title>
+                <Popover.Content>{content}</Popover.Content>
+            </Popover>
+        </>
+    )
+}
 
-export default InfoButton;
+export default InfoButton
