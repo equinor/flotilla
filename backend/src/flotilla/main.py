0e7e61fbc0e36bbbc4c18139f746055f924d33e2
Vetle Koppergård <vetk@equinor.com>
2022-01-21 13:47:42 +0100

    Add basic robots endpoints

--- backend/src/flotilla/main.py
+++ backend/src/flotilla/main.py
@@ -1,8 +1,19 @@
+from http.client import HTTPException
+
 from fastapi import FastAPI
+from flotilla_openapi.models.problem_details import ProblemDetails
+from sqlalchemy.orm import Session
+
+from flotilla.api.robots_api import router as robots_router
+from flotilla.database.db import Base, SessionLocal, engine
+from flotilla.database.mock_database.mock_database import populate_mock_db
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.on_event("startup")
+def startup_event():
+    populate_mock_db(session=SessionLocal(), engine=engine, base=Base)
+
+
+app.include_router(robots_router)
--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,11 +1,24 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from flotilla.main import app
 
-client = TestClient(app)
+
+def test_read_robots():
+    with TestClient(app) as client:
+        response = client.get("/robots")
+        assert response.status_code == 200
+        assert len(response.json()) == 2
 
 
-def test_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+@pytest.mark.parametrize(
+    "robot_id, expected_status_code",
+    [
+        (1, 200),
+        (23, 404),
+    ],
+)
+def test_read_robot(robot_id, expected_status_code):
+    with TestClient(app) as client:
+        response = client.get(f"/robots/{robot_id}")
+        assert response.status_code == expected_status_code
