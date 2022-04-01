9377233cc269df8a1955358cb18bdca47f151be6
Anette Uttisrud <anetteuttisrud@gmail.com>
2022-04-01 09:09:08 +0200

    Add event handler

---
+++ backend/src/flotilla/services/events/event_handler.py
@@ -0,0 +1,21 @@
+from logging import getLogger
+from token import OP
+from typing import List
+
+from pytest import Session
+
+from flotilla.database.crud import read_events
+from flotilla.database.db import SessionLocal
+from flotilla.database.models import EventDBModel
+
+logger = getLogger("event handler")
+
+
+def start_event_handler():
+    logger.info(f"Event handler started")
+    db_session: Session = SessionLocal()
+
+    db_events: List[EventDBModel] = read_events(db_session)
+
+    SessionLocal.remove()
+    logger.info(db_events)
