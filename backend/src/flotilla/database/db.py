cdc9d146a5fd2480d7ee09c764cc2731353b6444
Anette Uttisrud <anetteuttisrud@gmail.com>
2022-04-05 16:34:06 +0200

    Add SQLALCHEMY_DATABASE_URL to config

--- backend/src/flotilla/database/db.py
+++ backend/src/flotilla/database/db.py
@@ -2,11 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-# TODO: Move to config
-SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+from flotilla.settings import settings
 
 connection = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 ).connect()
 
 session_factory = sessionmaker(autocommit=False, autoflush=False, bind=connection)
