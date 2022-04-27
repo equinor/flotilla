0166a277d74edcaa7aca6441ad57f34751005058
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-04-27 16:41:19 +0200

    Add authorization by role

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -1,14 +1,29 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using api.Configurations;
 using api.Context;
 using api.Services;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<FlotillaDbContext>(options => options.UseInMemoryDatabase("flotilla"));
 builder.Services.AddScoped<RobotService>();
 builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwagger(builder.Configuration);  
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .RequireRole(builder.Configuration.GetSection("Authorization")["Roles"])
+        .Build();
+});
 
 var app = builder.Build();
 
@@ -16,11 +31,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => {
+        c.OAuthClientId(builder.Configuration["AzureAd:ClientId"]);
+        // The following parameter represents the "audience" of the access token.
+        c.OAuthAdditionalQueryStringParams(new Dictionary<string, string>{ {"Resource", builder.Configuration["AzureAd:ClientId"] } });
+        c.OAuthUsePkce();
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
