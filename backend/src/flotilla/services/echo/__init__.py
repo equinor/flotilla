7af063f4333dca89f1ba30b65e3fba751a04d85a
Christian de Jonge <chjo@equinor.com>
2022-04-27 10:34:27 +0200

    Delete python backend

--- backend/src/flotilla/services/echo/__init__.py
+++
@@ -1,8 +0,0 @@
-from .service import (
-    EchoDeserializerException,
-    EchoService,
-    EchoServiceInterface,
-    get_echo_service,
-    mission_deserializer,
-    tag_deserializer,
-)
--- backend/tests/test_db.py
+++
@@ -1,5 +0,0 @@
-from flotilla.database.models import RobotDBModel
-
-
-def test_mock_db(session):
-    assert len(session.query(RobotDBModel.name).all()) == 2
