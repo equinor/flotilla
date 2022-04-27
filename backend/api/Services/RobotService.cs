5912c84d487348a1a839274da4a5875fd57f234b
Christian de Jonge <chjo@equinor.com>
2022-04-27 13:59:15 +0200

    Add Robot endpoint and db

---
+++ backend/api/Services/RobotService.cs
@@ -0,0 +1,32 @@
+using api.Context;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Services
+{
+    public class RobotService
+    {
+        private readonly FlotillaDbContext _context;
+
+        public RobotService(FlotillaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Robot> Create(Robot robot)
+        {
+            await _context.Robots.AddAsync(robot);
+            await _context.SaveChangesAsync();
+            return robot;
+        }
+
+        public async Task<IEnumerable<Robot>> ReadAll()
+        {
+            return await _context.Robots.ToListAsync();
+        }
+
+        public async Task<Robot?> Read(string id)
+        {
+            return await _context.Robots.FirstOrDefaultAsync(robot => robot.Id.Equals(id));
+        }
+    }
+}
