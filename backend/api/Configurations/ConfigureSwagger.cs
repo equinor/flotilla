e14dcc4a52fb2453d67e1593433311562f29f0cb
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-04-27 16:41:19 +0200

    Add authorization by role

---
+++ backend/api/Configurations/ConfigureSwagger.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace api.Configurations
+{
+    public static class CustomServiceConfigurations
+    {
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                // Add Authorization button in UI
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        AuthorizationCode = new OpenApiOAuthFlow
+                        {
+                            TokenUrl = new Uri($"{configuration["AzureAd:Instance"]}/{configuration["AzureAd:TenantId"]}/oauth2/token"),
+                            AuthorizationUrl = new Uri($"{configuration["AzureAd:Instance"]}/{configuration["AzureAd:TenantId"]}/oauth2/authorize"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "api://ea4c7b92-47b3-45fb-bd25-a8070f0c495c/user_impersonation" , "User Impersonation" }
+                            },
+                        }
+                    }
+                });
+                // Show which endpoints have authorization in the UI
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
