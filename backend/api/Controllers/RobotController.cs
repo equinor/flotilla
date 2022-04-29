c248a1527435c06759adc62d7467ef00202abd6b
Christian de Jonge <chjo@equinor.com>
2022-04-29 11:11:41 +0200

    Add start and stop endpoints

--- backend/api/Controllers/RobotController.cs
+++ backend/api/Controllers/RobotController.cs
@@ -1,4 +1,5 @@
-﻿using Api.Models;
+﻿using System.Text.Json;
+using Api.Models;
 using Api.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,26 +11,21 @@ public class RobotController : ControllerBase
 {
     private readonly ILogger<RobotController> _logger;
     private readonly RobotService _robotService;
+    private readonly IsarService _isarService;
 
-    public RobotController(ILogger<RobotController> logger, RobotService robotService)
+    public RobotController(ILogger<RobotController> logger, RobotService robotService, IsarService isarService)
     {
         _logger = logger;
         _robotService = robotService;
+        _isarService = isarService;
     }
 
     /// <summary>
     /// List all robots on the asset.
     /// </summary>
     /// <remarks>
-    /// Responses are paginated.
-    ///
-    /// Example query:
-    ///
-    ///     /robots?
-    ///
     /// <para> This query gets all robots (paginated) </para>
     /// </remarks>
-    /// <returns> List of robots </returns>
     [HttpGet]
     [ProducesResponseType(typeof(IList<Robot>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -44,13 +40,8 @@ public class RobotController : ControllerBase
     /// Gets the robot with the specified id
     /// </summary>
     /// <remarks>
-    /// Example query:
-    ///
-    ///     /robots/{id}
-    ///
     /// <para> This query gets the robot with the specified id </para>
     /// </remarks>
-    /// <returns> Robot </returns>
     [HttpGet]
     [Route("{id}")]
     [ProducesResponseType(typeof(Robot), StatusCodes.Status200OK)]
@@ -61,10 +52,7 @@ public class RobotController : ControllerBase
     public async Task<ActionResult<Robot>> GetRobotById([FromRoute] string id)
     {
         var robot = await _robotService.Read(id);
-        if (robot == null)
-        {
-            return NotFound();
-        }
+        if (robot == null) return NotFound($"Could not find robot with id {id}");
         return Ok(robot);
     }
 
@@ -83,15 +71,56 @@ public class RobotController : ControllerBase
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult<Robot>> PostRobot([FromBody] Robot robot)
     {
-        try
+        var newRobot = await _robotService.Create(robot);
+        return CreatedAtAction(nameof(GetRobotById), new { id = newRobot.Id }, newRobot);
+    }
+
+    /// <summary>
+    /// Start a mission for a given robot
+    /// </summary>
+    /// <remarks>
+    /// <para> This query starts a mission for a given robot and creates a report </para>
+    /// </remarks>
+    [HttpPost]
+    [Route("{robotId}/start/{missionId}")]
+    [ProducesResponseType(typeof(Report), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<Report>> StartMission([FromRoute] string robotId, [FromRoute] string missionId)
+    {
+        var robot = await _robotService.Read(robotId);
+        if (robot == null) return NotFound($"Could not find robot with robot id {robotId}");
+        var report = await _isarService.StartMission(robot, missionId);
+        return Ok(report);
+    }
+
+    /// <summary>
+    /// Stop robot
+    /// </summary>
+    /// <remarks>
+    /// <para> This query stops a robot based on id </para>
+    /// </remarks>
+    [HttpPost]
+    [Route("{robotId}/stop/")]
+    [ProducesResponseType(typeof(IsarStopMissionResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<IsarStopMissionResponse>> StopMission([FromRoute] string robotId)
+    {
+        var response = await _isarService.StopMission();
+        if (!response.IsSuccessStatusCode) _logger.LogError("Could not stop mission with id {robotId}", robotId);
+        if (response.Content != null)
         {
-            var newRobot = await _robotService.Create(robot);
-            return CreatedAtAction(nameof(GetRobotById), new { id = newRobot.Id }, newRobot);
+            string? responseContent = await response.Content.ReadAsStringAsync();
+            var isarResponse = JsonSerializer.Deserialize<IsarStopMissionResponse>(responseContent);
+            return Ok(isarResponse);
         }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Unable to create robot");
-            throw;
-        }
+        return NotFound($"Could not stop mission on robot: {robotId}");
     }
 }
---
+++ backend/api/Database/Models/Report.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+# nullable disable
+namespace Api.Models
+{
+    public class Report
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public virtual Robot Robot { get; set; }
+
+        [MaxLength(128)]
+        [Required]
+        public string IsarMissionId { get; set; }
+
+        [MaxLength(128)]
+        [Required]
+        public string EchoMissionId { get; set; }
+
+        [MaxLength(128)]
+        [Required]
+        public string Log { get; set; }
+
+        [Required]
+        public ReportStatus ReportStatus { get; set; }
+
+        [Required]
+        public DateTimeOffset StartTime { get; set; }
+
+        public DateTimeOffset EndTime { get; set; }
+
+        [Required]
+        public virtual ICollection<ReportEntry> Entries { get; private set; }
+    }
+
+    public enum ReportStatus
+    {
+        InProgress, Completed, Failed
+    }
+}
