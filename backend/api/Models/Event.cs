5c209405c7c7952a671624a404aa8060806cb505
Anette Uttisrud <anetteuttisrud@gmail.com>
2022-05-04 16:24:59 +0200

    Add event controller

---
+++ backend/api/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+# nullable disable
+namespace Api.Models
+{
+    public class Event
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        [Required]
+        public virtual Robot Robot { get; set; }
+        [MaxLength(128)]
+        [Required]
+        public string IsarMissionId { get; set; }
+        [Required]
+        public DateTimeOffset StartTime { get; set; }
+        [Required]
+        public DateTimeOffset EndTime { get; set; }
+    }
+}
