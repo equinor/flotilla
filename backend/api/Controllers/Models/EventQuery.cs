9c91ad75a3b2dcc07aec0ea1c742b3876297f3b5
oysand <outb@equinor.com>
2022-05-12 15:09:42 +0200

    Reduce post events input requirements

---
+++ backend/api/Controllers/Models/EventQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Controllers.Models
+{
+    public struct EventQuery
+    {
+        public string RobotId { get; set; }
+        public string IsarMissionId { get; set; }
+        public DateTimeOffset? StartTime { get; set; }
+    }
+}
