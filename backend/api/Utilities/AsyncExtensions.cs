d59bba8767b7c1572dc9ee6faf5bc1ef10e5edfd
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-05-20 14:47:12 +0200

    Make cancellation token awaitable

---
+++ backend/api/Utilities/AsyncExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Api.Utilities
+{
+    public static class AsyncExtensions
+    {
+        /// <summary>
+        /// Allows a cancellation token to be awaited.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static CancellationTokenAwaiter GetAwaiter(this CancellationToken ct)
+        {
+            // return our special awaiter
+            return new CancellationTokenAwaiter { _cancellationToken = ct };
+        }
+
+        /// <summary>
+        /// The awaiter for cancellation tokens.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public struct CancellationTokenAwaiter : INotifyCompletion, ICriticalNotifyCompletion
+        {
+            public CancellationTokenAwaiter(CancellationToken cancellationToken)
+            {
+                _cancellationToken = cancellationToken;
+            }
+
+            internal CancellationToken _cancellationToken;
+
+            public object GetResult()
+            {
+                // this is called by compiler generated methods when the
+                // task has completed. Instead of returning a result, we
+                // just throw an exception.
+                if (IsCompleted)
+                    throw new OperationCanceledException();
+                else
+                    throw new InvalidOperationException(
+                        "The cancellation token has not yet been cancelled."
+                    );
+            }
+
+            // called by compiler generated/.net internals to check
+            // if the task has completed.
+            public bool IsCompleted => _cancellationToken.IsCancellationRequested;
+
+            // The compiler will generate stuff that hooks in
+            // here. We hook those methods directly into the
+            // cancellation token.
+            public void OnCompleted(Action continuation) =>
+                _cancellationToken.Register(continuation);
+
+            public void UnsafeOnCompleted(Action continuation) =>
+                _cancellationToken.Register(continuation);
+        }
+    }
+}
