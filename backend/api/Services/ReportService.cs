b630a830d26a16560be68d13507fda4be4efe39d
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-05-23 15:52:04 +0200

    Change event subscription pattern

--- backend/api/Services/ReportService.cs
+++ backend/api/Services/ReportService.cs
@@ -1,8 +1,5 @@
 ﻿using Api.Context;
 using Api.Models;
-using Api.Mqtt;
-using Api.Mqtt.Events;
-using Api.Mqtt.MessageModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Services
@@ -10,25 +7,10 @@ namespace Api.Services
     public class ReportService
     {
         private readonly FlotillaDbContext _context;
-        private readonly ILogger<ReportService> _logger;
 
-        public ReportService(FlotillaDbContext context, ILogger<ReportService> logger)
+        public ReportService(FlotillaDbContext context)
         {
             _context = context;
-            _logger = logger;
-            var mqtt = MqttService.Instance;
-
-            if (mqtt is not null)
-            {
-                mqtt.MqttIsarMissionReceived += OnMissionUpdate;
-                mqtt.MqttIsarTaskReceived += OnTaskUpdate;
-                mqtt.MqttIsarStepReceived += OnStepUpdate;
-            }
-            else
-                _logger.LogWarning(
-                    "Mqtt service not instantiated - Can't subscribe to events in service '{service}'",
-                    nameof(ReportService)
-                );
         }
 
         public async Task<Report> Create(Report report)
@@ -67,28 +49,6 @@ namespace Api.Services
             return await _context.Reports.FirstOrDefaultAsync(
                 report => report.Id.Equals(id, StringComparison.Ordinal)
             );
-        }
-
-        private void OnMissionUpdate(object? sender, MqttReceivedArgs mqttArgs)
-        {
-            var mission = (IsarMission)mqttArgs.Message;
-            _logger.LogInformation(
-                "{time} - Mission {id} updated",
-                mission.Timestamp,
-                mission.MissionId
-            );
-        }
-
-        private void OnTaskUpdate(object? sender, MqttReceivedArgs mqttArgs)
-        {
-            var task = (IsarTask)mqttArgs.Message;
-            _logger.LogInformation("{time} - Task {id} updated", task.Timestamp, task.TaskId);
-        }
-
-        private void OnStepUpdate(object? sender, MqttReceivedArgs mqttArgs)
-        {
-            var step = (IsarStep)mqttArgs.Message;
-            _logger.LogInformation("{time} - Step {id} updated", step.Timestamp, step.StepId);
         }
     }
 }
