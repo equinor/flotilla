737808b4564d37f81eb62bf95992cad9591dbb14
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-06-10 12:05:50 +0200

    Fix dockerization of backend

---
+++ backend/api/Configurations/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Api.Configurations
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        /// <summary>
+        /// Creates the AZURE_CLIENT_ID and AZURE_TENANT_ID configuration values for the
+        /// <see href="https://docs.microsoft.com/en-us/dotnet/api/azure.identity.environmentcredential?view=azure-dotnet">Environment Credentials</see>
+        /// used by the application when dockerized.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static void AddAzureEnvironmentVariables(this WebApplicationBuilder builder)
+        {
+            string? clientId = builder.Configuration
+                .GetSection("AzureAd")
+                .GetValue<string?>("ClientId");
+            if (clientId is not null)
+            {
+                Environment.SetEnvironmentVariable("AZURE_CLIENT_ID", clientId);
+                Console.WriteLine("'AZURE_CLIENT_ID' set to " + clientId);
+            }
+
+            string? tenantId = builder.Configuration
+                .GetSection("AzureAd")
+                .GetValue<string?>("TenantId");
+            if (clientId is not null)
+            {
+                Environment.SetEnvironmentVariable("AZURE_TENANT_ID", tenantId);
+                Console.WriteLine("'AZURE_TENANT_ID' set to " + tenantId);
+            }
+        }
+    }
+}
