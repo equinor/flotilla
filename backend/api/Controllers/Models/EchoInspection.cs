02c271617c882fc3fdb2584984f7606c6339ddbf
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-07-13 10:23:54 +0200

    Add class 'EchoInspection'

---
+++ backend/api/Controllers/Models/EchoInspection.cs
@@ -0,0 +1,17 @@
+﻿using static Api.Database.Models.IsarStep;
+
+namespace Api.Controllers.Models
+{
+    public class EchoInspection
+    {
+        public InspectionTypeEnum InspectionType { get; set; }
+
+        public float? TimeInSeconds { get; set; }
+
+        public EchoInspection(InspectionTypeEnum inspectionType, float? timeInSeconds)
+        {
+            InspectionType = inspectionType;
+            TimeInSeconds = timeInSeconds;
+        }
+    }
+}
