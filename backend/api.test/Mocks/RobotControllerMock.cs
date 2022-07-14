02b9100127da27df796ca21982fb5ca5314165f5
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-07-14 11:42:04 +0200

    Add RobotControllerMock helper

---
+++ backend/api.test/Mocks/RobotControllerMock.cs
@@ -0,0 +1,31 @@
+﻿using Api.Controllers;
+using Api.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Api.Test.Mocks
+{
+    internal class RobotControllerMock
+    {
+        public Mock<IIsarService> IsarServiceMock;
+        public Mock<IEchoService> EchoServiceMock;
+        public Mock<IRobotService> RobotServiceMock;
+        public Mock<IReportService> ReportServiceMock;
+        public Mock<RobotController> Mock;
+
+        public RobotControllerMock()
+        {
+            ReportServiceMock = new Mock<IReportService>();
+            IsarServiceMock = new Mock<IIsarService>();
+            EchoServiceMock = new Mock<IEchoService>();
+            RobotServiceMock = new Mock<IRobotService>();
+
+            var mockLoggerController = new Mock<ILogger<RobotController>>();
+
+            Mock = new Mock<RobotController>(mockLoggerController.Object, RobotServiceMock.Object, IsarServiceMock.Object, EchoServiceMock.Object)
+            {
+                CallBase = true
+            };
+        }
+    }
+}
