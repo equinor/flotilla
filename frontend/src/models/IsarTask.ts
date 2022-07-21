becda47c2d963f19e63ca5f3c3c50b8730164a91
Morten Fredriksen <morfre@equinor.com>
2022-07-21 13:36:41 +0200

    Add frontend models for report

---
+++ frontend/src/models/IsarTask.ts
@@ -0,0 +1,23 @@
+import { IsarStep } from "./IsarStep"
+import { Report } from "./Report"
+
+export interface IsarTask{
+    Id: string 
+    IsarTaskId: string 
+    Report: Report 
+    TagId: string 
+    TaskStatus: IsarTaskStatus 
+    Time: Date 
+    Steps: IsarStep[] 
+}
+
+enum IsarTaskStatus
+{
+    Successful,
+    PartiallySuccessful,
+    NotStarted,
+    InProgress,
+    Failed,
+    Cancelled,
+    Paused,
+}
