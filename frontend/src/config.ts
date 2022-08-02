8efd19f941b642d60b0521dfed1de783ab3ef8ab
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-08-02 14:40:26 +0200

    Configure environment variables for frontend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,14 +1,22 @@
-function GetEnvVariable(name: string): string {
-    const value = process.env[name]
+const globalVars = window as any
 
-    if (value === undefined)
-        throw new Error(`Env variable "${name}" is not set. Verify that your .env file is up to date with .env.example`)
-    else return value as string
+function GetEnvVariable(name: string): string {
+    const value = globalVars[name]
+    if (value === '' || value === undefined) {
+        console.warn('Global variable named "' + name + '" undefined or empty. Attempting to use env variable.')
+        const env_value = process.env[name]
+
+        if (env_value === undefined)
+            throw new Error(
+                `Env variable "${name}" is not set. Verify that your .env file is up to date with .env.example`
+            )
+        else return env_value as string
+    } else return value as string
 }
 
 export const config = {
-    BACKEND_URL: GetEnvVariable('REACT_APP_BACKEND_URL'),
-    FRONTEND_URL: GetEnvVariable('REACT_APP_FRONTEND_URL'),
-    AD_CLIENT_ID: GetEnvVariable('REACT_APP_AD_CLIENT_ID'),
-    AD_TENANT_ID: GetEnvVariable('REACT_APP_AD_TENANT_ID'),
+    BACKEND_URL: GetEnvVariable('BACKEND_URL'),
+    FRONTEND_URL: GetEnvVariable('FRONTEND_URL'),
+    AD_CLIENT_ID: GetEnvVariable('AD_CLIENT_ID'),
+    AD_TENANT_ID: GetEnvVariable('AD_TENANT_ID'),
 }
