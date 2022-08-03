ac51aa39559df4f49959eeb751efde6c143999f5
oysand <outb@equinor.com>
2022-08-03 12:38:09 +0200

    Change videostream to interface

--- frontend/src/models/VideoStream.ts
+++ frontend/src/models/VideoStream.ts
@@ -1,13 +1,6 @@
-export class VideoStream {
-    id?: string = 'defaultId'
-    name?: string = 'defaultName'
-    robotId?: string = 'defaltRobotId'
-    url?: string = 'defaultUrl'
-
-    constructor(id?: string, name?: string, robotId?: string, url?: string) {
-        this.id = id
-        this.name = name
-        this.robotId = robotId
-        this.url = url
-    }
+export interface VideoStream {
+    id?: string
+    name?: string
+    robotId?: string
+    url: string
 }
