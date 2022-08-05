5a832c994e57331221b98b46f1379c19bb722c5a
oysand <outb@equinor.com>
2022-08-05 13:11:24 +0200

    Improve small video sections

--- frontend/src/components/VideoStream/VideoStreamWindow.tsx
+++ frontend/src/components/VideoStream/VideoStreamWindow.tsx
@@ -1,7 +1,9 @@
-import { Card } from '@equinor/eds-core-react'
+import { Button, Card, Icon, Typography } from '@equinor/eds-core-react'
 import { tokens } from '@equinor/eds-tokens'
+import { fullscreen } from '@equinor/eds-icons'
 import styled from 'styled-components'
 import { VideoStream } from 'models/VideoStream'
+import { useState } from 'react'
 
 interface VideoStreamProps {
     videoStream: VideoStream
@@ -9,13 +11,42 @@ interface VideoStreamProps {
 
 const VideoCard = styled(Card)`
     width: 400px;
-    height: 225px;
+    padding: 16px;
 `
 
+const VideoSection = styled(Card)`
+    display: inline-flex;
+`
+
+const FullscreenButton = styled(Button)`
+    position: absolute;
+    position: absolute;
+    bottom: 8px;
+    right: 8px;
+    width: 32px;
+    height: 32px;
+    opacity: 0.75;
+`
+
+Icon.add({ fullscreen })
+
+
+
 export function VideoStreamWindow({ videoStream }: VideoStreamProps) {
+    const [fullScreenMode, setFullScreenMode] = useState<boolean>(false)
+    const toggleFullScreenMode = () => {
+        setFullScreenMode(!fullScreenMode)
+    }
+
     return (
         <VideoCard variant="default" style={{ boxShadow: tokens.elevation.sticky }}>
-            <img src={videoStream.url} />
-        </VideoCard>
+            <VideoSection>
+                <img src={videoStream.url} alt="robot video stream" />
+                <FullscreenButton color="secondary" onClick={toggleFullScreenMode}>
+                    <Icon name="fullscreen" size={32} />
+                </FullscreenButton>
+            </VideoSection>
+            <Typography variant="h5">{videoStream.name}</Typography>
+        </VideoCard >
     )
 }
