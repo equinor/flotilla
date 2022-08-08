48363055e547537e9910b8b5a87d0afaeb219414
oysand <outb@equinor.com>
2022-08-08 15:36:44 +0200

    Add fullscreen video functionality

---
+++ frontend/src/components/VideoStream/VideoSection.tsx
@@ -0,0 +1,37 @@
+import { Button, Card, Icon } from '@equinor/eds-core-react'
+import { fullscreen } from '@equinor/eds-icons'
+import styled from 'styled-components'
+
+import { VideoStream } from 'models/VideoStream'
+
+interface VideoSectionProps {
+    videoStream: VideoStream
+    toggleFullScreenModeFunction: VoidFunction
+}
+
+const StyledVideoSection = styled(Card)`
+    height: 95%;
+    display: inline-flex;
+`
+
+const FullscreenButton = styled(Button)`
+    position: absolute;
+    bottom: 8px;
+    right: 8px;
+    width: 32px;
+    height: 32px;
+    opacity: 0.75;
+`
+
+Icon.add({ fullscreen })
+
+export function VideoSection({ videoStream, toggleFullScreenModeFunction }: VideoSectionProps) {
+    return (
+        <StyledVideoSection>
+            <img src={videoStream.url} width="100%" height="100%" alt={videoStream.name + ' video stream'} />
+            <FullscreenButton color="secondary" onClick={toggleFullScreenModeFunction}>
+                <Icon name="fullscreen" size={32} />
+            </FullscreenButton>
+        </StyledVideoSection>
+    )
+}
--- frontend/src/components/VideoStream/VideoStreamWindow.tsx
+++ frontend/src/components/VideoStream/VideoStreamWindow.tsx
@@ -1,50 +1,53 @@
-import { Button, Card, Icon, Typography } from '@equinor/eds-core-react'
+import { Card, Icon, Typography } from '@equinor/eds-core-react'
 import { tokens } from '@equinor/eds-tokens'
 import { fullscreen } from '@equinor/eds-icons'
 import styled from 'styled-components'
-import { VideoStream } from 'models/VideoStream'
 import { useState } from 'react'
+import ReactModal from 'react-modal'
 
-interface VideoStreamProps {
+import { VideoStream } from 'models/VideoStream'
+import { VideoSection } from './VideoSection'
+
+interface VideoStreamWindowProps {
     videoStream: VideoStream
 }
-
 const VideoCard = styled(Card)`
-    width: 400px;
     padding: 16px;
 `
 
-const VideoSection = styled(Card)`
-    display: inline-flex;
-`
-
-const FullscreenButton = styled(Button)`
+const VideoFullScreen = styled(ReactModal)`
     position: absolute;
-    position: absolute;
-    bottom: 8px;
-    right: 8px;
-    width: 32px;
-    height: 32px;
-    opacity: 0.75;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `
 
 Icon.add({ fullscreen })
 
-export function VideoStreamWindow({ videoStream }: VideoStreamProps) {
+export function VideoStreamWindow({ videoStream }: VideoStreamWindowProps) {
     const [fullScreenMode, setFullScreenMode] = useState<boolean>(false)
+
     const toggleFullScreenMode = () => {
         setFullScreenMode(!fullScreenMode)
     }
 
     return (
-        <VideoCard variant="default" style={{ boxShadow: tokens.elevation.sticky }}>
-            <VideoSection>
-                <img src={videoStream.url} alt="robot video stream" />
-                <FullscreenButton color="secondary" onClick={toggleFullScreenMode}>
-                    <Icon name="fullscreen" size={32} />
-                </FullscreenButton>
-            </VideoSection>
-            <Typography variant="h5">{videoStream.name}</Typography>
-        </VideoCard>
+        <>
+            <VideoCard variant="default" style={{ boxShadow: tokens.elevation.sticky }}>
+                <VideoSection videoStream={videoStream} toggleFullScreenModeFunction={toggleFullScreenMode} />
+                <Typography variant="h5">{videoStream.name}</Typography>
+            </VideoCard>
+            <VideoFullScreen
+                isOpen={fullScreenMode}
+                onRequestClose={() => {
+                    setFullScreenMode(false)
+                }}
+            >
+                <VideoCard variant="default" style={{ boxShadow: tokens.elevation.sticky }}>
+                    <Typography variant="h5">{videoStream.name}</Typography>
+                    <VideoSection videoStream={videoStream} toggleFullScreenModeFunction={toggleFullScreenMode} />
+                </VideoCard>
+            </VideoFullScreen>
+        </>
     )
 }
