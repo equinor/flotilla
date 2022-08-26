083fa477f4ef6ede85d40cb674c6849362d97785
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-08-26 13:12:22 +0200

    Update tests

---
+++ backend/api.test/Controllers/TestMissionController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using Api.Controllers;
+using Api.Database.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Api.Test.Controllers
+{
+    [Collection("Database collection")]
+    public class TestMissionController
+    {
+        private readonly MissionController _controller;
+
+        public TestMissionController(DatabaseFixture fixture)
+        {
+            var missionLogger = new Mock<ILogger<MissionService>>().Object;
+            var missionControllerLogger = new Mock<ILogger<MissionController>>().Object;
+            var context = fixture.NewContext;
+            var missionService = new MissionService(context, missionLogger);
+            var robotService = new RobotService(context);
+            var echoService = new Mock<IEchoService>().Object;
+
+            _controller = new MissionController(
+                missionService,
+                robotService,
+                echoService,
+                missionControllerLogger
+            );
+        }
+
+        [Fact]
+        public void GetMissions()
+        {
+            var result = (OkObjectResult)_controller.GetMissions(null, null).Result.Result!;
+            var scheduledMissions = (IList<Mission>?)result.Value;
+
+            Assert.NotNull(scheduledMissions);
+            if (scheduledMissions is null)
+                return;
+        }
+
+        [Fact]
+        public void GetMissionById_ShouldReturnNotFound()
+        {
+            var actionResultType = typeof(NotFoundObjectResult);
+            string missionId = "RandomString";
+
+            IActionResult? result = _controller.GetMissionById(missionId).Result.Result;
+
+            // Check if the result is, or inherits from, the expected result type
+            Assert.IsAssignableFrom(actionResultType, result);
+        }
+
+        [Fact]
+        public void GetMissionById_ShouldReturnOkObject()
+        {
+            var actionResultType = typeof(OkObjectResult);
+            var result = (OkObjectResult)_controller.GetMissions(null, null).Result.Result!;
+            var scheduledMissions = (IList<Mission>?)result.Value;
+            Assert.NotNull(scheduledMissions);
+
+            if (scheduledMissions is null)
+                return;
+
+            string missionId = scheduledMissions[0].Id;
+
+            IActionResult? actionResult = _controller.GetMissionById(missionId).Result.Result;
+
+            // Check if the result is, or inherits from, the expected result type
+            Assert.IsAssignableFrom(actionResultType, actionResult);
+        }
+
+        [Fact]
+        public void DeleteMission_ShouldReturnNotFound()
+        {
+            var actionResultType = typeof(NotFoundObjectResult);
+            string missionId = "RandomString";
+
+            IActionResult? result = _controller.DeleteMission(missionId).Result.Result;
+
+            // Check if the result is, or inherits from, the expected result type
+            Assert.IsAssignableFrom(actionResultType, result);
+        }
+
+        [Fact]
+        public void DeleteMission_ShouldReturnOkObject()
+        {
+            var actionResultType = typeof(OkObjectResult);
+            var result = (OkObjectResult)_controller.GetMissions(null, null).Result.Result!;
+            var scheduledMissions = (IList<Mission>?)result.Value;
+            Assert.NotNull(scheduledMissions);
+
+            if (scheduledMissions is null)
+                return;
+
+            string scheduledMissionsId = scheduledMissions[0].Id;
+            IActionResult? actionResult = _controller
+                .DeleteMission(scheduledMissionsId)
+                .Result.Result;
+
+            // Check if the result is, or inherits from, the expected result type
+            Assert.IsAssignableFrom(actionResultType, actionResult);
+        }
+    }
+}
