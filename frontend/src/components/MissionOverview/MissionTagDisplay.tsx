39f3ddfb3f411f20e1c91961491e2b26806e7ad9
Morten Fredriksen <morfre@equinor.com>
2022-09-21 12:11:51 +0200

    Tally remaining tasks on ongoing mission card

--- frontend/src/components/MissionOverview/MissionTagDisplay.tsx
+++ frontend/src/components/MissionOverview/MissionTagDisplay.tsx
@@ -1,14 +1,24 @@
 import { Typography } from '@equinor/eds-core-react'
+import { IsarTask, IsarTaskStatus } from 'models/IsarTask'
 import styled from 'styled-components'
 
 const StyledTagCount = styled.div`
     display: flex;
 `
+interface TaskProps {
+    tasks: IsarTask[]
+}
 
-export function MissionProgressDisplay() {
+export function MissionProgressDisplay({tasks}: TaskProps) {
+    var numberOfTasks = tasks.length
+    var numberOfCompletedTasks = 0
+    tasks.forEach((task) => {
+        if(task.taskStatus == IsarTaskStatus.Successful)
+            numberOfCompletedTasks++
+    })
     return (
         <StyledTagCount>
-            <Typography>Tag ?/?</Typography>
+            <Typography>Tag {numberOfCompletedTasks}/{numberOfTasks}</Typography>
         </StyledTagCount>
     )
 }
