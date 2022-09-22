ab6e50f4b84636b99a4ad3facae662ccf23ef06f
aestene <arnts@equinor.com>
2022-09-22 13:45:04 +0200

    Set broker certificate based on environment
    This means an environment variable TLS_SERVER_KEY is required to start
    the broker docker container. Thus the certificate key is not stored in
    the image itself and not retrievable if the image is lost.
    
    The image may now be published publicly.

---
+++ broker/entrypoint.sh
@@ -0,0 +1,12 @@
+# Set certificate key based on environment variable
+# TLS_SERVER_KEY
+
+start='-----BEGIN PRIVATE KEY-----'
+end='-----END PRIVATE KEY-----'
+
+
+echo ${start} > mosquitto/config/certs/server-key.pem
+echo ${TLS_SERVER_KEY} >> mosquitto/config/certs/server-key.pem
+echo ${end} >> mosquitto/config/certs/server-key.pem
+
+mosquitto -p 1883 -c mosquitto/config/mosquitto.conf
