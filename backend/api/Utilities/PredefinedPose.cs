d1f035419e5410dd2358670ecf884904024f9ace
oysand <outb@equinor.com>
2022-09-23 16:43:08 +0200

    Implement predefined pose workaround

---
+++ backend/api/Utilities/PredefinedPose.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Api.Database.Models;
+namespace Api.Utilities
+{
+    public class PredefinedPose
+    {
+        [Required]
+        public string Tag { get; set; }
+        public Pose Pose { get; set; }
+        public PredefinedPose(string tag, Pose pose)
+        {
+            Tag = tag;
+            Pose = pose;
+        }
+    }
+}
