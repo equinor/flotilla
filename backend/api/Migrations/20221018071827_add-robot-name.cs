479a7dad1a37002a9a5fd1444904881a63d81214
aestene <arnts@equinor.com>
2022-10-18 09:19:48 +0200

    Migrate database to add mission name

---
+++ backend/api/Migrations/20221018071827_add-robot-name.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class addrobotname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Missions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Missions");
+        }
+    }
+}
