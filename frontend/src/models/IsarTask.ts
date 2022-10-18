c6577eabc8264f4a7a8c5cdc90b31857d51e1d4a
aestene <arnts@equinor.com>
2022-10-18 14:01:30 +0200

    Update progress indicator based on task status

--- frontend/src/models/IsarTask.ts
+++ frontend/src/models/IsarTask.ts
@@ -11,12 +11,26 @@ export interface IsarTask {
     steps: IsarStep[]
 }
 
-enum IsarTaskStatus {
-    Successful,
-    PartiallySuccessful,
-    NotStarted,
-    InProgress,
-    Failed,
-    Cancelled,
-    Paused,
+export enum IsarTaskStatus {
+    Successful = 'Successful',
+    PartiallySuccessful = 'PartiallySuccessful',
+    NotStarted = 'NotStarted',
+    InProgress = 'InProgress',
+    Failed = 'Failed',
+    Cancelled = 'Cancelled',
+    Paused = 'Paused',
+}
+
+export namespace IsarTaskStatus {
+    export function isComplete(status: IsarTaskStatus): boolean {
+        if (
+            status === IsarTaskStatus.Successful ||
+            status === IsarTaskStatus.PartiallySuccessful ||
+            status === IsarTaskStatus.Failed ||
+            status === IsarTaskStatus.Cancelled
+        ) {
+            return true
+        }
+        return false
+    }
 }
