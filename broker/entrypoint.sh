4f2c7c8053f310a3a25514200785bbc357cd6b0f
Christian de Jonge <chjo@equinor.com>
2022-11-03 10:34:44 +0100

    Remove single quotes from server key

--- broker/entrypoint.sh
+++ broker/entrypoint.sh
@@ -1,12 +1,11 @@
+#!/bin/sh
 # Set certificate key based on environment variable
 # TLS_SERVER_KEY
 
 start='-----BEGIN PRIVATE KEY-----'
 end='-----END PRIVATE KEY-----'
-
-
 echo ${start} > mosquitto/config/certs/server-key.pem
-echo ${TLS_SERVER_KEY} >> mosquitto/config/certs/server-key.pem
+echo ${TLS_SERVER_KEY} | tr -d "'" >> mosquitto/config/certs/server-key.pem
 echo ${end} >> mosquitto/config/certs/server-key.pem
 
 mosquitto -p 1883 -c mosquitto/config/mosquitto.conf
