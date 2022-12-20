7b7d7e3cce4c137bb6a79248b6607e293460a4b9
Peter Hatlebrekke Husebø <phhu@equinor.com>
2022-12-20 15:05:41 +0100

    Use videojs placeholder

--- frontend/src/components/VideoStream/VideoStreamWindow.tsx
+++ frontend/src/components/VideoStream/VideoStreamWindow.tsx
@@ -1,23 +1,9 @@
-import { Card, Typography, Icon } from '@equinor/eds-core-react'
-import { videocam_off } from '@equinor/eds-icons'
+import { Card, Typography } from '@equinor/eds-core-react'
 import { tokens } from '@equinor/eds-tokens'
 import styled from 'styled-components'
 
 import { VideoStream } from 'models/VideoStream'
 import VideoPlayer from './Video'
-
-Icon.add({ videocam_off })
-
-const NoVideoPlaceholder = styled(Icon)`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-right: -50%;
-    transform: translate(-50%, -50%);
-    width: 50%;
-    height: 50%;
-    opacity: 0.4;
-`
 
 const VideoCard = styled(Card)`
     padding: 16px;
@@ -47,19 +33,9 @@ export function VideoStreamWindow({ videoStream }: VideoStreamWindowProps) {
     return (
         <VideoCard variant="default" style={{ boxShadow: tokens.elevation.raised }}>
             <StyledVideoSection>
-                {ValidateVideoStream(videoStream) && <VideoPlayer options={videoJsOptions} />}
-                {!ValidateVideoStream(videoStream) && <NoVideoPlaceholder name="videocam_off" />}
+                <VideoPlayer options={videoJsOptions} />
             </StyledVideoSection>
             <Typography variant="h5">{videoStream.name}</Typography>
         </VideoCard>
     )
-}
-
-function ValidateVideoStream(videoStream: VideoStream) {
-    const stream = new Image()
-    stream.src = videoStream.url
-    if (stream.naturalHeight === 0) {
-        return false
-    }
-    return true
 }
