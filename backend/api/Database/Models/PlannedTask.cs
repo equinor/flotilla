2846f4dd3cc7834e6ecab19ca6aa5e1e0748c85a
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-02-08 15:50:33 +0100

    Change ISAR models to structs and remove nesting

--- backend/api/Database/Models/PlannedTask.cs
+++ backend/api/Database/Models/PlannedTask.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using Api.Controllers.Models;
 using Microsoft.EntityFrameworkCore;
-using static Api.Controllers.Models.IsarTaskDefinition;
 
 #nullable disable
 
