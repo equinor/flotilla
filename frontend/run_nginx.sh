dde702a7a15bc02e2826e0391afb7cfeff55a6c2
Thomas Sundvoll <thomas@sundvoll.net>
2023-02-16 07:07:39 +0100

    Make Backend API scope configurable

--- frontend/run_nginx.sh
+++ frontend/run_nginx.sh
@@ -2,6 +2,7 @@
 # Substitute environment variables in the index.html file using the values in the current container environment
 envsubst '
   ${REACT_APP_BACKEND_URL}
+  ${REACT_APP_BACKEND_API_SCOPE}
   ${REACT_APP_FRONTEND_URL}
   ${REACT_APP_AD_CLIENT_ID}
   ${REACT_APP_AD_TENANT_ID}
