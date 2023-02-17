409f89623471fddb9c0c77e1db477e2b7e28debd
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-02-17 16:30:06 +0100

    add migrations

---
+++ backend/api/Migrations/20230217152941_test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldRotate270Clockwise",
+                table: "VideoStream",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShouldRotate270Clockwise",
+                table: "VideoStream");
+        }
+    }
+}
