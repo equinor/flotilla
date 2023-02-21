be08004ea23aa9f37931aae195285ec9fe68e361
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-02-21 10:14:37 +0100

    Add migrations for new field order

---
+++ backend/api/Migrations/20230221091332_Add order field to tasks.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class Addorderfieldtotasks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldRotate270Clockwise",
+                table: "VideoStream",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlanOrder",
+                table: "PlannedTask",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShouldRotate270Clockwise",
+                table: "VideoStream");
+
+            migrationBuilder.DropColumn(
+                name: "PlanOrder",
+                table: "PlannedTask");
+        }
+    }
+}
