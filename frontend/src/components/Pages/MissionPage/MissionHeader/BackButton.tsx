4224ff8f75b87646522c03f41ab1497b6cc5f3ba
Eddasol <esole@equinor.com>
2023-03-06 10:59:31 +0100

    Modify back button to redirect to previous page

--- frontend/src/components/Pages/MissionPage/MissionHeader/BackButton.tsx
+++ frontend/src/components/Pages/MissionPage/MissionHeader/BackButton.tsx
@@ -2,6 +2,7 @@ import { Button, Icon } from '@equinor/eds-core-react'
 import { arrow_back } from '@equinor/eds-icons'
 import styled from 'styled-components'
 import { Text } from 'components/Contexts/LanguageContext'
+import { useNavigate } from 'react-router-dom'
 
 Icon.add({ arrow_back })
 
@@ -11,8 +12,9 @@ const StyledButton = styled(Button)`
 `
 
 export function BackButton() {
+    let navigate = useNavigate()
     return (
-        <StyledButton variant="ghost" href="..">
+        <StyledButton variant="ghost" onClick={() => navigate(-1)}>
             <Icon name="arrow_back" size={16} />
             {Text('Back')}
         </StyledButton>
