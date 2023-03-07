f3fcf671c2c7c2471ed6f46acd3cf943ad82ae7d
Peter Hatlebrekke Husebø <PHHU@equinor.com>
2023-03-07 14:43:21 +0100

    Add Unauthorized page

---
+++ frontend/src/components/Pages/UnauthorizedPage/GoToAccessITButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@equinor/eds-core-react'
+
+export const GoToAccessITButton = () => {
+    return (
+        <Button href="https://accessit.equinor.com" variant="contained">
+            Go to AccessIT
+        </Button>
+    )
+}
