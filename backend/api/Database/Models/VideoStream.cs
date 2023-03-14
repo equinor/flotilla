293513af7e3cdd85ca6c6cb7f86373d3973f436a
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-03-14 11:09:38 +0100

    Make VideoStream comparable

--- backend/api/Database/Models/VideoStream.cs
+++ backend/api/Database/Models/VideoStream.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Api.Database.Models
 {
     [Owned]
-    public class VideoStream
+    public class VideoStream : IEquatable<VideoStream>
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -25,5 +25,45 @@ namespace Api.Database.Models
         public string Type { get; set; }
 
         public bool ShouldRotate270Clockwise { get; set; }
+
+        public bool Equals(VideoStream other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Name == other.Name && Url == other.Url && Type == other.Type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return Equals((VideoStream)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Url, Type);
+        }
     }
 }
