dd68f4f0b3646f7349c41d8795483e3173eeea27
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-03-16 15:32:29 +0100

    Fix migrations for IsarId

---
+++ backend/api/Migrations/20230316143159_AddRobotIsarId.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class AddRobotIsarId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IsarId",
+                table: "Robots",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsarId",
+                table: "Robots");
+        }
+    }
+}
