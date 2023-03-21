d5c547e867975a650efda819adcfd6611e5e13f5
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-03-21 17:07:14 +0100

    Show status reason in frontend

---
+++ frontend/src/components/Pages/MissionPage/StatusReason.tsx
@@ -0,0 +1,40 @@
+import { Card, Typography } from '@equinor/eds-core-react'
+import { tokens } from '@equinor/eds-tokens'
+import { Mission, MissionStatus } from 'models/Mission'
+import styled from 'styled-components'
+
+const StyledCard = styled(Card)`
+    width: fit-content;
+    padding: 7px 15px;
+    gap: 0.2rem;
+`
+
+interface MissionProps {
+    mission: Mission
+}
+
+export function StatusReason({ mission }: MissionProps) {
+    if (mission.statusReason === null) return <></>
+
+    var warningLevel: 'default' | 'info' | 'warning' | 'danger' = 'info'
+    switch (mission.status) {
+        case MissionStatus.Failed:
+            warningLevel = 'danger'
+            break
+        case MissionStatus.Aborted:
+            warningLevel = 'danger'
+            break
+        case MissionStatus.Cancelled:
+            warningLevel = 'warning'
+            break
+        case MissionStatus.PartiallySuccessful:
+            warningLevel = 'warning'
+            break
+    }
+
+    return (
+        <StyledCard variant={warningLevel} style={{ boxShadow: tokens.elevation.raised }}>
+            <Typography variant="h5">{mission.statusReason}</Typography>
+        </StyledCard>
+    )
+}
