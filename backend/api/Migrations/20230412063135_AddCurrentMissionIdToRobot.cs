228446c17bfc1b14aa185854f34d19fa3590f543
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-04-12 08:32:53 +0200

    Add migrations for CurrentMissionId on Robot

---
+++ backend/api/Migrations/20230412063135_AddCurrentMissionIdToRobot.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class AddCurrentMissionIdToRobot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Logs",
+                table: "Robots");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentMissionId",
+                table: "Robots",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EchoPoseId",
+                table: "MissionTask",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "EstimatedDuration",
+                table: "Missions",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AssetCode",
+                table: "Missions",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentMissionId",
+                table: "Robots");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Logs",
+                table: "Robots",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EchoPoseId",
+                table: "MissionTask",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "EstimatedDuration",
+                table: "Missions",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AssetCode",
+                table: "Missions",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
