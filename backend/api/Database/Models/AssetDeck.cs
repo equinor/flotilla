6f7cc5b2b37609174a602aa19bfa12a4f659c4c8
Eddasol <esole@equinor.com>
2023-04-12 14:08:04 +0200

    Add assetDeckService

---
+++ backend/api/Database/Models/AssetDeck.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+namespace Api.Database.Models
+{
+    public class AssetDeck
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string AssetCode { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string DeckName { get; set; }
+
+        [Required]
+        public Pose DefaultLocalizationPose { get; set; }
+    }
+}
