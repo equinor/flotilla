19ea23d9d0c8758f152dcbb9bcdfe850cbc97953
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-04-14 14:37:03 +0200

    Check for new access token regularly

--- frontend/src/components/Contexts/AuthProvider.tsx
+++ frontend/src/components/Contexts/AuthProvider.tsx
@@ -10,14 +10,22 @@ type Props = {
 export const AuthContext = createContext('')
 
 export const AuthProvider = (props: Props) => {
+    // Check for new token every 5 seconds
+    const tokenRefreshInterval = 5000
     const handleError = useErrorHandler()
     const msalContext = useMsal()
     const [accessToken, setAccessToken] = useState('')
-    useEffect(() => {
+    const UpdateToken = () => {
         fetchAccessToken(msalContext).then((accessToken) => {
             setAccessToken(accessToken)
         })
-        //.catch((e) => handleError(e))
-    })
+    }
+    UpdateToken()
+    useEffect(() => {
+        const id = setInterval(() => {
+            UpdateToken()
+        }, tokenRefreshInterval)
+        return () => clearInterval(id)
+    }, [])
     return <AuthContext.Provider value={accessToken}>{props.children}</AuthContext.Provider>
 }
