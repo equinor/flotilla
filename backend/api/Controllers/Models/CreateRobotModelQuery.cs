3a7e7cddffae7808ca54c0344a31546a5f9058f0
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-04-20 13:10:42 +0200

    Add robot model controller and service

---
+++ backend/api/Controllers/Models/CreateRobotModelQuery.cs
@@ -0,0 +1,27 @@
+﻿using Api.Database.Models;
+
+namespace Api.Controllers.Models
+{
+    public struct CreateRobotModelQuery
+    {
+        /// <summary>
+        /// The type of robot model
+        /// </summary>
+        public RobotType RobotType { get; set; }
+
+        /// <summary>
+        /// Lower battery warning threshold in percentage
+        /// </summary>
+        public float? BatteryWarningThreshold { get; set; }
+
+        /// <summary>
+        /// Upper pressure warning threshold in mBar
+        /// </summary>
+        public float? UpperPressureWarningThreshold { get; set; }
+
+        /// <summary>
+        /// Lower pressure warning threshold in mBar
+        /// </summary>
+        public float? LowerPressureWarningThreshold { get; set; }
+    }
+}
---
+++ backend/api/Services/RobotModelService.cs
@@ -0,0 +1,86 @@
+﻿using Api.Database.Context;
+using Api.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Services
+{
+    public interface IRobotModelService
+    {
+        public abstract Task<IEnumerable<RobotModel>> ReadAll();
+
+        public abstract Task<RobotModel?> ReadById(string id);
+
+        public abstract Task<RobotModel?> ReadByRobotType(RobotType robotType);
+
+        public abstract Task<RobotModel> Create(RobotModel newRobotModel);
+
+        public abstract Task<RobotModel> Update(RobotModel robotModel);
+
+        public abstract Task<RobotModel?> Delete(string id);
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Globalization",
+        "CA1309:Use ordinal StringComparison",
+        Justification = "EF Core refrains from translating string comparison overloads to SQL"
+    )]
+    public class RobotModelService : IRobotModelService
+    {
+        private readonly FlotillaDbContext _context;
+
+        public RobotModelService(FlotillaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<RobotModel>> ReadAll()
+        {
+            return await GetRobotModels().ToListAsync();
+        }
+
+        private IQueryable<RobotModel> GetRobotModels()
+        {
+            return _context.RobotModels;
+        }
+
+        public async Task<RobotModel?> ReadById(string id)
+        {
+            return await GetRobotModels()
+                .FirstOrDefaultAsync(robotModel => robotModel.Id.Equals(id));
+        }
+
+        public async Task<RobotModel?> ReadByRobotType(RobotType robotType)
+        {
+            return await GetRobotModels()
+                .FirstOrDefaultAsync(robotModel => robotModel.Type.Equals(robotType));
+        }
+
+        public async Task<RobotModel> Create(RobotModel newRobotModel)
+        {
+            await _context.RobotModels.AddAsync(newRobotModel);
+            await _context.SaveChangesAsync();
+            return newRobotModel;
+        }
+
+        public async Task<RobotModel> Update(RobotModel robotModel)
+        {
+            var entry = _context.Update(robotModel);
+            await _context.SaveChangesAsync();
+            return entry.Entity;
+        }
+
+        public async Task<RobotModel?> Delete(string id)
+        {
+            var robotModel = await GetRobotModels().FirstOrDefaultAsync(ev => ev.Id.Equals(id));
+            if (robotModel is null)
+            {
+                return null;
+            }
+
+            _context.RobotModels.Remove(robotModel);
+            await _context.SaveChangesAsync();
+
+            return robotModel;
+        }
+    }
+}
