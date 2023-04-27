b1d1c194fb21aa8d95475dc744e79a7afe1ae2d5
Eddasol <esole@equinor.com>
2023-04-27 08:07:50 +0200

    Add optional description to tasks

--- backend/api/Controllers/Models/CustomMissionQuery.cs
+++ backend/api/Controllers/Models/CustomMissionQuery.cs
@@ -19,6 +19,8 @@ namespace Api.Controllers.Models
 
         public string? TagId { get; set; }
 
+        public string? Description { get; set; }
+
         public Pose RobotPose { get; set; }
 
         public List<CustomInspectionQuery> Inspections { get; set; }
--- backend/api/Migrations/20230427061444_AddTaskDescription.Designer.cs
+++ backend/api/Migrations/20230427061444_AddTaskDescription.Designer.cs
@@ -4,6 +4,7 @@ using Api.Database.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api.Migrations
 {
     [DbContext(typeof(FlotillaDbContext))]
-    partial class FlotillaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230427061444_AddTaskDescription")]
+    partial class AddTaskDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
