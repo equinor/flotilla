b878f83c2fb303f858b19841b2aca02849397085
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-05-08 10:58:06 +0200

    Add migrations for AverageDurationPerTag

---
+++ backend/api/Migrations/20230508085737_AddAverageDurationPerTagToRobotModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class AddAverageDurationPerTagToRobotModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "AverageDurationPerTag",
+                table: "RobotModels",
+                type: "real",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageDurationPerTag",
+                table: "RobotModels");
+        }
+    }
+}
