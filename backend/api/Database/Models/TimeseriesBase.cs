5e5370859bd94a8460d34e50f309e55136458c29
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-05-15 14:20:35 +0200

    Add timeseries models

---
+++ backend/api/Database/Models/TimeseriesBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#pragma warning disable CS8618
+namespace Api.Database.Models
+{
+    public class TimeseriesBase
+    {
+        [Required]
+        public DateTimeOffset Time { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Robot))]
+        public string RobotId { get; set; }
+
+        public string? MissionId { get; set; }
+    }
+}
