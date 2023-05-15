669f38924d03082d9e53059c41026aace24b0397
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-05-15 14:20:35 +0200

    Add timeseries models

---
+++ backend/api/Database/Models/RobotBatteryTimeseries.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Database.Models
+{
+    [Keyless]
+    public class RobotBatteryTimeseries : TimeseriesBase
+    {
+        [Required]
+        public float BatteryLevel { get; set; }
+    }
+}
---
+++ backend/api/Database/Models/RobotPressureTimeseries.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Database.Models
+{
+    [Keyless]
+    public class RobotPressureTimeseries : TimeseriesBase
+    {
+        [Required]
+        public float Pressure { get; set; }
+    }
+}
