e6989751f7c1f3d76ceb50963883feb051b0b45c
Anders Chirico Indrebø <ACIN@equinor.com>
2023-05-30 15:31:42 +0200

    Add new context to store mission control

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,25 @@ import { AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-reac
 import { SignInPage } from './components/Pages/SignInPage/SignInPage'
 import { FlotillaSite } from 'components/Pages/FlotillaSite'
 import { LanguageProvider } from 'components/Contexts/LanguageContext'
+import { MissionControlProvider } from 'components/Contexts/MissionControlContext'
 
 function App() {
     return (
         <LanguageProvider>
-            {/* // <ErrorBoundary fallbackRender={({ error, resetErrorBoundary }) => ErrorFallback(error)}> */}
-            <>
-                <UnauthenticatedTemplate>
-                    <div className="sign-in-page">
-                        <SignInPage></SignInPage>
-                    </div>
-                </UnauthenticatedTemplate>
-                <AuthenticatedTemplate>
-                    <FlotillaSite />
-                </AuthenticatedTemplate>
-            </>
-            {/* </ErrorBoundary> */}
+            <MissionControlProvider>
+                {/* // <ErrorBoundary fallbackRender={({ error, resetErrorBoundary }) => ErrorFallback(error)}> */}
+                <>
+                    <UnauthenticatedTemplate>
+                        <div className="sign-in-page">
+                            <SignInPage></SignInPage>
+                        </div>
+                    </UnauthenticatedTemplate>
+                    <AuthenticatedTemplate>
+                        <FlotillaSite />
+                    </AuthenticatedTemplate>
+                </>
+                {/* </ErrorBoundary> */}
+            </MissionControlProvider>
         </LanguageProvider>
     )
 }
---
+++ frontend/src/components/Contexts/MissionControlContext.tsx
@@ -0,0 +1,74 @@
+import { createContext, useContext, useState, FC } from 'react'
+import { BackendAPICaller } from 'api/ApiCaller'
+import { Mission } from 'models/Mission'
+import { ControlButton } from 'components/Pages/FrontPage/MissionOverview/MissionControlButtons'
+
+interface IMissionControlState {
+    isWaitingForResponse: boolean
+}
+
+interface Props {
+    children: React.ReactNode
+}
+
+export interface IMissionControlContext {
+    missionControlState: IMissionControlState
+    setIsWaitingForResponse: (isWaiting: boolean) => void
+    handleClick: (button: ControlButton, mission: Mission) => void
+}
+
+const defaultMissionControlInterface = {
+    missionControlState: { isWaitingForResponse: false },
+    setIsWaitingForResponse: (isWaiting: boolean) => {},
+    handleClick: (button: ControlButton, mission: Mission) => {},
+}
+
+export const MissionControlContext = createContext<IMissionControlContext>(defaultMissionControlInterface)
+const defaultManagementState: IMissionControlState = {
+    isWaitingForResponse: false,
+}
+
+export const MissionControlProvider: FC<Props> = ({ children }) => {
+    const [missionControlState, setMissionControlState] = useState<IMissionControlState>(defaultManagementState)
+
+    const setIsWaitingForResponse = (isWaiting: boolean) => {
+        setMissionControlState({ isWaitingForResponse: isWaiting })
+    }
+
+    const handleClick = (button: ControlButton, mission: Mission) => {
+        switch (button) {
+            case ControlButton.Pause: {
+                setIsWaitingForResponse(true)
+                BackendAPICaller.pauseMission(mission.robot.id).then((_) => setIsWaitingForResponse(false))
+                //.catch((e) => handleError(e))
+                break
+            }
+            case ControlButton.Resume: {
+                setIsWaitingForResponse(true)
+                BackendAPICaller.resumeMission(mission.robot.id).then((_) => setIsWaitingForResponse(false))
+                //.catch((e) => handleError(e))
+                break
+            }
+            case ControlButton.Stop: {
+                setIsWaitingForResponse(true)
+                BackendAPICaller.stopMission(mission.robot.id).then((_) => setIsWaitingForResponse(false))
+                //.catch((e) => handleError(e))
+                break
+            }
+        }
+    }
+
+    return (
+        <MissionControlContext.Provider
+            value={{
+                missionControlState,
+                setIsWaitingForResponse,
+                handleClick,
+            }}
+        >
+            {children}
+        </MissionControlContext.Provider>
+    )
+}
+
+export const useMissionControlContext = () => useContext(MissionControlContext) as IMissionControlContext
