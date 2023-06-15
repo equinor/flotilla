d694041a6ca8d7b6a7924b686233dbdcb7467f0c
Ole Drange <odran@equinor.com>
2023-06-15 08:57:02 +0200

    Change language selector to be EN NO
    Add EN NO language selector
    
    Fix language selector
    
    Make bold

---
+++ frontend/src/components/Header/LanguageSelector.tsx
@@ -0,0 +1,60 @@
+import { useLanguageContext } from 'components/Contexts/LanguageContext'
+import { useState } from 'react'
+import styled from 'styled-components'
+import { Button, Divider } from '@equinor/eds-core-react'
+
+const LanguageContainer = styled.div`
+    display: flex;
+    gap: 0rem;
+`
+
+const LanguageButton = styled(Button)<{ $language?: string; $text?: string }>`
+    background: none;
+    padding: 0;
+    font: inherit;
+    color: #6f6f6f;
+    border-color: transparent;
+    outline: none;
+    font-weight: bold;
+    ${(props) =>
+        props.$text === props.$language
+            ? `
+        color: #007079;
+        text-decoration: underline;
+      &:hover {
+          cursor: default;
+          background: none;
+        }`
+            : `
+         &:hover {
+        cursor: pointer;
+        background:#DEEDEE;
+    }
+      `}
+`
+
+const VerticalBar = styled(Divider)`
+    width: 2px;
+    height: 10px;
+    margin: 13px 3px 1px 3px;
+`
+
+export function SelectLanguage() {
+    const { language, switchLanguage } = useLanguageContext()
+    const [newLanguage, setNewLanguage] = useState<string>()
+    const handleLanguageChange = (selectedLanguage: string) => {
+        switchLanguage(selectedLanguage)
+        setNewLanguage(selectedLanguage)
+    }
+    return (
+        <LanguageContainer>
+            <LanguageButton $text="en" $language={language} onClick={() => handleLanguageChange('en')}>
+                EN
+            </LanguageButton>
+            <VerticalBar />
+            <LanguageButton $text="no" $language={language} onClick={() => handleLanguageChange('no')}>
+                NO
+            </LanguageButton>
+        </LanguageContainer>
+    )
+}
