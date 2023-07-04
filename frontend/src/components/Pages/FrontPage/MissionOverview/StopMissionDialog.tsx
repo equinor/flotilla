3b56cec2ddc2f489787c90bd248b0a61729c9f2a
Mariana R. Santos <mrica@equinor.com>
2023-07-04 15:53:21 +0200

    Add confirmation dialog to stop mission button

---
+++ frontend/src/components/Pages/FrontPage/MissionOverview/StopMissionDialog.tsx
@@ -0,0 +1,102 @@
+import { Button, Dialog, Typography, Icon } from '@equinor/eds-core-react'
+import styled from 'styled-components'
+import { TranslateText } from 'components/Contexts/LanguageContext'
+import { Icons } from 'utils/icons'
+import { useState, useEffect } from 'react'
+import { tokens } from '@equinor/eds-tokens'
+import { Mission } from 'models/Mission'
+import { useMissionControlContext } from 'components/Contexts/MissionControlContext'
+
+const StyledDisplayButtons = styled.div`
+    display: flex;
+    width: 410px;
+    flex-direction: columns;
+    justify-content: flex-end;
+    gap: 0.5rem;
+`
+
+const StyledDialog = styled(Dialog)`
+    display: grid;
+    width: 450px;
+`
+
+const StyledText = styled.div`
+    display: grid;
+    gird-template-rows: auto, auto;
+    gap: 1rem;
+`
+
+interface MissionProps {
+    mission: Mission
+}
+
+export enum ControlButton {
+    Pause,
+    Stop,
+    Resume,
+}
+
+export const StopMissionDialog = ({ mission }: MissionProps): JSX.Element => {
+    const [isStopMissionDialogOpen, setIsStopMissionDialogOpen] = useState<boolean>(false)
+    const [missionId, setMissionId] = useState<string>()
+    const { handleClick } = useMissionControlContext()
+
+    const openDialog = () => {
+        setIsStopMissionDialogOpen(true)
+        setMissionId(mission.id)
+    }
+
+    useEffect(() => {
+        if (missionId !== mission.id) setIsStopMissionDialogOpen(false)
+    }, [mission.id])
+
+    return (
+        <>
+            <Button variant="ghost_icon" onClick={openDialog}>
+                <Icon
+                    name={Icons.StopButton}
+                    style={{ color: tokens.colors.interactive.secondary__resting.rgba }}
+                    size={40}
+                />
+            </Button>
+
+            <StyledDialog open={isStopMissionDialogOpen} isDismissable>
+                <Dialog.Header>
+                    <Dialog.Title>
+                        <Typography variant="h5">
+                            {TranslateText('Stop mission')} <strong>'{mission.name}'</strong>?{' '}
+                        </Typography>
+                    </Dialog.Title>
+                </Dialog.Header>
+                <Dialog.CustomContent>
+                    <StyledText>
+                        <Typography variant="body_long">{TranslateText('Stop button pressed warning text')}</Typography>
+                        <Typography variant="body_long">
+                            {TranslateText('Stop button pressed confirmation text')}
+                        </Typography>
+                    </StyledText>
+                </Dialog.CustomContent>
+                <Dialog.Actions>
+                    <StyledDisplayButtons>
+                        <Button
+                            variant="outlined"
+                            color="danger"
+                            onClick={() => {
+                                setIsStopMissionDialogOpen(false)
+                            }}
+                        >
+                            {TranslateText('Cancel')}
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="danger"
+                            onClick={() => handleClick(ControlButton.Stop, mission)}
+                        >
+                            {TranslateText('Stop mission')}
+                        </Button>
+                    </StyledDisplayButtons>
+                </Dialog.Actions>
+            </StyledDialog>
+        </>
+    )
+}
