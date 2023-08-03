f686d2c801e168052201e772db8721cafd464c78
andchiind <aindreboe@gmail.com>
2023-08-03 12:43:27 +0200

    Add small incremental migrations

---
+++ backend/api/Migrations/20230803103020_RenameAssetDeckToArea.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class RenameAssetDeckToArea : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SafePositions_AssetDecks_AssetDeckId",
+                table: "SafePositions");
+
+            migrationBuilder.RenameColumn(
+                name: "AssetDeckId",
+                table: "SafePositions",
+                newName: "AreaId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SafePositions_AssetDeckId",
+                table: "SafePositions",
+                newName: "IX_SafePositions_AreaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SafePositions_AssetDecks_AreaId",
+                table: "SafePositions",
+                column: "AreaId",
+                principalTable: "AssetDecks",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SafePositions_AssetDecks_AreaId",
+                table: "SafePositions");
+
+            migrationBuilder.RenameColumn(
+                name: "AreaId",
+                table: "SafePositions",
+                newName: "AssetDeckId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SafePositions_AreaId",
+                table: "SafePositions",
+                newName: "IX_SafePositions_AssetDeckId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SafePositions_AssetDecks_AssetDeckId",
+                table: "SafePositions",
+                column: "AssetDeckId",
+                principalTable: "AssetDecks",
+                principalColumn: "Id");
+        }
+    }
+}
