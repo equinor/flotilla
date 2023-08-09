ac95e1aa474c615f813d37c2e7027e9d499b5fdc
aestene <arnts@equinor.com>
2023-08-09 13:50:31 +0200

    Remove redundant abstract keywords

--- backend/api/Services/MissionRunService.cs
+++ backend/api/Services/MissionRunService.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq.Dynamic.Core;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 using Api.Controllers.Models;
 using Api.Database.Context;
@@ -7,53 +7,52 @@ using Api.Services.Models;
 using Api.Utilities;
 using Microsoft.EntityFrameworkCore;
 using TaskStatus = Api.Database.Models.TaskStatus;
-
 namespace Api.Services
 {
     public interface IMissionRunService
     {
-        public abstract Task<MissionRun> Create(MissionRun missionRun);
+        public Task<MissionRun> Create(MissionRun missionRun);
 
-        public abstract Task<PagedList<MissionRun>> ReadAll(MissionRunQueryStringParameters parameters);
+        public Task<PagedList<MissionRun>> ReadAll(MissionRunQueryStringParameters parameters);
 
-        public abstract Task<MissionRun?> ReadById(string id);
+        public Task<MissionRun?> ReadById(string id);
 
-        public abstract Task<MissionRun?> ReadNextScheduledRunByMissionId(string missionId);
+        public Task<MissionRun?> ReadNextScheduledRunByMissionId(string missionId);
 
-        public abstract Task<MissionRun> Update(MissionRun mission);
+        public Task<MissionRun> Update(MissionRun mission);
 
-        public abstract Task<MissionRun?> UpdateMissionRunStatusByIsarMissionId(
+        public Task<MissionRun?> UpdateMissionRunStatusByIsarMissionId(
             string isarMissionId,
             MissionStatus missionStatus
         );
 
-        public abstract Task<bool> UpdateTaskStatusByIsarTaskId(
+        public Task<bool> UpdateTaskStatusByIsarTaskId(
             string isarMissionId,
             string isarTaskId,
             IsarTaskStatus taskStatus
         );
 
-        public abstract Task<bool> UpdateStepStatusByIsarStepId(
+        public Task<bool> UpdateStepStatusByIsarStepId(
             string isarMissionId,
             string isarTaskId,
             string isarStepId,
             IsarStepStatus stepStatus
         );
 
-        public abstract Task<MissionRun?> Delete(string id);
+        public Task<MissionRun?> Delete(string id);
     }
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+    [SuppressMessage(
         "Globalization",
         "CA1309:Use ordinal StringComparison",
         Justification = "EF Core refrains from translating string comparison overloads to SQL"
     )]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+    [SuppressMessage(
         "Globalization",
         "CA1304:Specify CultureInfo",
         Justification = "Entity framework does not support translating culture info to SQL calls"
     )]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+    [SuppressMessage(
         "Globalization",
         "CA1307:Specify CultureInfo",
         Justification = "Entity framework does not support translating culture info to SQL calls"
@@ -67,23 +66,6 @@ namespace Api.Services
         {
             _context = context;
             _logger = logger;
-        }
-
-        private IQueryable<MissionRun> GetMissionRunsWithSubModels()
-        {
-            return _context.MissionRuns
-                .Include(missionRun => missionRun.Area)
-                .ThenInclude(area => area.Deck)
-                .ThenInclude(deck => deck.Plant)
-                .ThenInclude(plant => plant.Installation)
-                .Include(missionRun => missionRun.Robot)
-                .ThenInclude(robot => robot.VideoStreams)
-                .Include(missionRun => missionRun.Robot)
-                .ThenInclude(robot => robot.Model)
-                .Include(missionRun => missionRun.Tasks)
-                .ThenInclude(planTask => planTask.Inspections)
-                .Include(missionRun => missionRun.Tasks)
-                .ThenInclude(task => task.Inspections);
         }
 
         public async Task<MissionRun> Create(MissionRun missionRun)
@@ -148,6 +130,164 @@ namespace Api.Services
             await _context.SaveChangesAsync();
 
             return missionRun;
+        }
+
+        private IQueryable<MissionRun> GetMissionRunsWithSubModels()
+        {
+            return _context.MissionRuns
+                .Include(missionRun => missionRun.Area)
+                .ThenInclude(area => area.Deck)
+                .ThenInclude(deck => deck.Plant)
+                .ThenInclude(plant => plant.Installation)
+                .Include(missionRun => missionRun.Robot)
+                .ThenInclude(robot => robot.VideoStreams)
+                .Include(missionRun => missionRun.Robot)
+                .ThenInclude(robot => robot.Model)
+                .Include(missionRun => missionRun.Tasks)
+                .ThenInclude(planTask => planTask.Inspections)
+                .Include(missionRun => missionRun.Tasks)
+                .ThenInclude(task => task.Inspections);
+        }
+
+        private static void SearchByName(ref IQueryable<MissionRun> missionRuns, string? name)
+        {
+            if (!missionRuns.Any() || string.IsNullOrWhiteSpace(name))
+            {
+                return;
+            }
+
+            missionRuns = missionRuns.Where(
+                missionRun =>
+                    missionRun.Name != null && missionRun.Name.ToLower().Contains(name.Trim().ToLower())
+            );
+        }
+
+        private static void SearchByRobotName(ref IQueryable<MissionRun> missionRuns, string? robotName)
+        {
+            if (!missionRuns.Any() || string.IsNullOrWhiteSpace(robotName))
+            {
+                return;
+            }
+
+            missionRuns = missionRuns.Where(
+                missionRun => missionRun.Robot.Name.ToLower().Contains(robotName.Trim().ToLower())
+            );
+        }
+
+        private static void SearchByTag(ref IQueryable<MissionRun> missionRuns, string? tag)
+        {
+            if (!missionRuns.Any() || string.IsNullOrWhiteSpace(tag))
+            {
+                return;
+            }
+
+            missionRuns = missionRuns.Where(
+                missionRun =>
+                    missionRun.Tasks.Any(
+                        task =>
+                            task.TagId != null
+                            && task.TagId.ToLower().Contains(tag.Trim().ToLower())
+                    )
+            );
+        }
+
+        /// <summary>
+        ///     Filters by <see cref="MissionRunQueryStringParameters.InstallationCode" /> and
+        ///     <see cref="MissionRunQueryStringParameters.Status" />
+        ///     <para>
+        ///         Uses LINQ Expression trees (see
+        ///         <seealso href="https://docs.microsoft.com/en-us/dotnet/csharp/expression-trees" />)
+        ///     </para>
+        /// </summary>
+        /// <param name="parameters"> The variable containing the filter params </param>
+        private static Expression<Func<MissionRun, bool>> ConstructFilter(
+            MissionRunQueryStringParameters parameters
+        )
+        {
+            Expression<Func<MissionRun, bool>> areaFilter = parameters.Area is null
+                ? missionRun => true
+                : missionRun =>
+                    missionRun.Area.Name.ToLower().Equals(parameters.Area.Trim().ToLower());
+
+            Expression<Func<MissionRun, bool>> installationFilter = parameters.InstallationCode is null
+                ? missionRun => true
+                : missionRun =>
+                    missionRun.InstallationCode.ToLower().Equals(parameters.InstallationCode.Trim().ToLower());
+
+            Expression<Func<MissionRun, bool>> statusFilter = parameters.Statuses is null
+                ? mission => true
+                : mission => parameters.Statuses.Contains(mission.Status);
+
+            Expression<Func<MissionRun, bool>> robotTypeFilter = parameters.RobotModelType is null
+                ? missionRun => true
+                : missionRun => missionRun.Robot.Model.Type.Equals(parameters.RobotModelType);
+
+            Expression<Func<MissionRun, bool>> robotIdFilter = parameters.RobotId is null
+                ? missionRun => true
+                : missionRun => missionRun.Robot.Id.Equals(parameters.RobotId);
+
+            Expression<Func<MissionRun, bool>> inspectionTypeFilter = parameters.InspectionTypes is null
+                ? mission => true
+                : mission => mission.Tasks.Any(
+                    task =>
+                        task.Inspections.Any(
+                            inspection => parameters.InspectionTypes.Contains(inspection.InspectionType)
+                        )
+                );
+
+            var minStartTime = DateTimeOffset.FromUnixTimeSeconds(parameters.MinStartTime);
+            var maxStartTime = DateTimeOffset.FromUnixTimeSeconds(parameters.MaxStartTime);
+            Expression<Func<MissionRun, bool>> startTimeFilter = missionRun =>
+                missionRun.StartTime == null
+                || DateTimeOffset.Compare(missionRun.StartTime.Value, minStartTime) >= 0
+                && DateTimeOffset.Compare(missionRun.StartTime.Value, maxStartTime) <= 0;
+
+            var minEndTime = DateTimeOffset.FromUnixTimeSeconds(parameters.MinEndTime);
+            var maxEndTime = DateTimeOffset.FromUnixTimeSeconds(parameters.MaxEndTime);
+            Expression<Func<MissionRun, bool>> endTimeFilter = missionRun =>
+                missionRun.EndTime == null
+                || DateTimeOffset.Compare(missionRun.EndTime.Value, minEndTime) >= 0
+                && DateTimeOffset.Compare(missionRun.EndTime.Value, maxEndTime) <= 0;
+
+            var minDesiredStartTime = DateTimeOffset.FromUnixTimeSeconds(
+                parameters.MinDesiredStartTime
+            );
+            var maxDesiredStartTime = DateTimeOffset.FromUnixTimeSeconds(
+                parameters.MaxDesiredStartTime
+            );
+            Expression<Func<MissionRun, bool>> desiredStartTimeFilter = missionRun =>
+                DateTimeOffset.Compare(missionRun.DesiredStartTime, minDesiredStartTime) >= 0
+                && DateTimeOffset.Compare(missionRun.DesiredStartTime, maxDesiredStartTime) <= 0;
+
+            // The parameter of the filter expression
+            var missionRun = Expression.Parameter(typeof(MissionRun));
+
+            // Combining the body of the filters to create the combined filter, using invoke to force parameter substitution
+            Expression body = Expression.AndAlso(
+                Expression.Invoke(installationFilter, missionRun),
+                Expression.AndAlso(
+                    Expression.Invoke(statusFilter, missionRun),
+                    Expression.AndAlso(
+                        Expression.Invoke(robotIdFilter, missionRun),
+                        Expression.AndAlso(
+                            Expression.Invoke(inspectionTypeFilter, missionRun),
+                            Expression.AndAlso(
+                                Expression.Invoke(desiredStartTimeFilter, missionRun),
+                                Expression.AndAlso(
+                                    Expression.Invoke(startTimeFilter, missionRun),
+                                    Expression.AndAlso(
+                                        Expression.Invoke(endTimeFilter, missionRun),
+                                        Expression.Invoke(robotTypeFilter, missionRun)
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+
+            // Constructing the resulting lambda expression by combining parameter and body
+            return Expression.Lambda<Func<MissionRun, bool>>(body, missionRun);
         }
 
         #region ISAR Specific methods
@@ -282,140 +422,5 @@ namespace Api.Services
 
         #endregion ISAR Specific methods
 
-        private static void SearchByName(ref IQueryable<MissionRun> missionRuns, string? name)
-        {
-            if (!missionRuns.Any() || string.IsNullOrWhiteSpace(name))
-                return;
-
-            missionRuns = missionRuns.Where(
-                missionRun =>
-                    missionRun.Name != null && missionRun.Name.ToLower().Contains(name.Trim().ToLower())
-            );
-        }
-
-        private static void SearchByRobotName(ref IQueryable<MissionRun> missionRuns, string? robotName)
-        {
-            if (!missionRuns.Any() || string.IsNullOrWhiteSpace(robotName))
-                return;
-
-            missionRuns = missionRuns.Where(
-                missionRun => missionRun.Robot.Name.ToLower().Contains(robotName.Trim().ToLower())
-            );
-        }
-
-        private static void SearchByTag(ref IQueryable<MissionRun> missionRuns, string? tag)
-        {
-            if (!missionRuns.Any() || string.IsNullOrWhiteSpace(tag))
-                return;
-
-            missionRuns = missionRuns.Where(
-                missionRun =>
-                    missionRun.Tasks.Any(
-                        task =>
-                            task.TagId != null
-                            && task.TagId.ToLower().Contains(tag.Trim().ToLower())
-                    )
-            );
-        }
-
-        /// <summary>
-        /// Filters by <see cref="MissionRunQueryStringParameters.InstallationCode"/> and <see cref="MissionRunQueryStringParameters.Status"/>
-        ///
-        /// <para>Uses LINQ Expression trees (see <seealso href="https://docs.microsoft.com/en-us/dotnet/csharp/expression-trees"/>)</para>
-        /// </summary>
-        /// <param name="parameters"> The variable containing the filter params </param>
-        private static Expression<Func<MissionRun, bool>> ConstructFilter(
-            MissionRunQueryStringParameters parameters
-        )
-        {
-            Expression<Func<MissionRun, bool>> areaFilter = parameters.Area is null
-                ? missionRun => true
-                : missionRun =>
-                      missionRun.Area.Name.ToLower().Equals(parameters.Area.Trim().ToLower());
-
-            Expression<Func<MissionRun, bool>> installationFilter = parameters.InstallationCode is null
-                ? missionRun => true
-                : missionRun =>
-                      missionRun.InstallationCode.ToLower().Equals(parameters.InstallationCode.Trim().ToLower());
-
-            Expression<Func<MissionRun, bool>> statusFilter = parameters.Statuses is null
-                ? mission => true
-                : mission => parameters.Statuses.Contains(mission.Status);
-
-            Expression<Func<MissionRun, bool>> robotTypeFilter = parameters.RobotModelType is null
-                ? missionRun => true
-                : missionRun => missionRun.Robot.Model.Type.Equals(parameters.RobotModelType);
-
-            Expression<Func<MissionRun, bool>> robotIdFilter = parameters.RobotId is null
-                ? missionRun => true
-                : missionRun => missionRun.Robot.Id.Equals(parameters.RobotId);
-
-            Expression<Func<MissionRun, bool>> inspectionTypeFilter = parameters.InspectionTypes is null
-                ? mission => true
-                : mission => mission.Tasks.Any(
-                        task =>
-                            task.Inspections.Any(
-                                inspection => parameters.InspectionTypes.Contains(inspection.InspectionType)
-                            )
-                );
-
-            var minStartTime = DateTimeOffset.FromUnixTimeSeconds(parameters.MinStartTime);
-            var maxStartTime = DateTimeOffset.FromUnixTimeSeconds(parameters.MaxStartTime);
-            Expression<Func<MissionRun, bool>> startTimeFilter = missionRun =>
-                missionRun.StartTime == null
-                || (
-                    DateTimeOffset.Compare(missionRun.StartTime.Value, minStartTime) >= 0
-                    && DateTimeOffset.Compare(missionRun.StartTime.Value, maxStartTime) <= 0
-                );
-
-            var minEndTime = DateTimeOffset.FromUnixTimeSeconds(parameters.MinEndTime);
-            var maxEndTime = DateTimeOffset.FromUnixTimeSeconds(parameters.MaxEndTime);
-            Expression<Func<MissionRun, bool>> endTimeFilter = missionRun =>
-                missionRun.EndTime == null
-                || (
-                    DateTimeOffset.Compare(missionRun.EndTime.Value, minEndTime) >= 0
-                    && DateTimeOffset.Compare(missionRun.EndTime.Value, maxEndTime) <= 0
-                );
-
-            var minDesiredStartTime = DateTimeOffset.FromUnixTimeSeconds(
-                parameters.MinDesiredStartTime
-            );
-            var maxDesiredStartTime = DateTimeOffset.FromUnixTimeSeconds(
-                parameters.MaxDesiredStartTime
-            );
-            Expression<Func<MissionRun, bool>> desiredStartTimeFilter = missionRun =>
-                DateTimeOffset.Compare(missionRun.DesiredStartTime, minDesiredStartTime) >= 0
-                && DateTimeOffset.Compare(missionRun.DesiredStartTime, maxDesiredStartTime) <= 0;
-
-            // The parameter of the filter expression
-            var missionRun = Expression.Parameter(typeof(MissionRun));
-
-            // Combining the body of the filters to create the combined filter, using invoke to force parameter substitution
-            Expression body = Expression.AndAlso(
-                Expression.Invoke(installationFilter, missionRun),
-                Expression.AndAlso(
-                    Expression.Invoke(statusFilter, missionRun),
-                    Expression.AndAlso(
-                        Expression.Invoke(robotIdFilter, missionRun),
-                        Expression.AndAlso(
-                            Expression.Invoke(inspectionTypeFilter, missionRun),
-                            Expression.AndAlso(
-                                Expression.Invoke(desiredStartTimeFilter, missionRun),
-                                Expression.AndAlso(
-                                    Expression.Invoke(startTimeFilter, missionRun),
-                                    Expression.AndAlso(
-                                        Expression.Invoke(endTimeFilter, missionRun),
-                                        Expression.Invoke(robotTypeFilter, missionRun)
-                                    )
-                                )
-                            )
-                        )
-                    )
-                )
-            );
-
-            // Constructing the resulting lambda expression by combining parameter and body
-            return Expression.Lambda<Func<MissionRun, bool>>(body, missionRun);
-        }
     }
 }
