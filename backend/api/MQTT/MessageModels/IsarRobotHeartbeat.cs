d86b39b67754f5c36d70e84c757a62325e271e24
aestene <arnts@equinor.com>
2023-08-09 10:46:03 +0200

    Add message model for heartbeat topic

---
+++ backend/api/MQTT/MessageModels/IsarRobotHeartbeat.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+namespace Api.Mqtt.MessageModels
+{
+#nullable disable
+    public class IsarRobotHeartbeat : MqttMessage
+    {
+        [JsonPropertyName("robot_name")]
+        public string RobotName { get; set; }
+
+        [JsonPropertyName("isar_id")]
+        public string IsarId { get; set; }
+
+        [JsonPropertyName("timestamp")]
+        public DateTime Timestamp { get; set; }
+    }
+}
