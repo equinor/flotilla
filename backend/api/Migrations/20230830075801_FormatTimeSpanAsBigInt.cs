a71c89febb55a3f9ff4a0f13ac6451455f424e1f
Anders Chirico Indrebø <ACIN@equinor.com>
2023-08-30 09:30:21 +0200

    Format TimeSpan as bigint in SQL

---
+++ backend/api/Migrations/20230830075801_FormatTimeSpanAsBigInt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FormatTimeSpanAsBigInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "InspectionFrequency",
+                table: "MissionDefinitions",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InspectionFrequency",
+                table: "MissionDefinitions");
+        }
+    }
+}
