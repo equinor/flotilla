29e1295366435314844153a7b8d0e8f0f85d694c
Mariana R. Santos <mrica@equinor.com>
2023-09-01 09:47:22 +0200

    Add stop all robots backend endpoints

--- backend/api/Controllers/EmergencyActionController.cs
+++ backend/api/Controllers/EmergencyActionController.cs
@@ -1,4 +1,5 @@
-﻿using Api.Controllers.Models;
+﻿using System.Globalization;
+using Api.Controllers.Models;
 using Api.Database.Models;
 using Api.Services;
 using Api.Services.Events;
@@ -65,11 +66,46 @@ namespace Api.Controllers
         }
 
         /// <summary>
+        ///     This endpoint will abort the current running mission run and attempt to return the robot to a safe position in the
+        ///     area. The mission run queue for the robot will be frozen and no further missions will run until the emergency
+        ///     action has been reversed.
+        /// </summary>
+        /// <remarks>
+        ///     <para> The endpoint fires an event which is then processed to stop the robot and schedule the next mission </para>
+        /// </remarks>
+        [HttpPost]
+        [Route("{installationCode}/abort-current-missions-and-send-all-robots-to-safe-zone")]
+        [Authorize(Roles = Role.User)]
+        [ProducesResponseType(typeof(MissionRun), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult AbortCurrentMissionAndSendAllRobotsToSafeZone(
+            [FromRoute] string installationCode)
+        {
+
+            var robots = _robotService.ReadAll().Result
+                            .Where(a =>
+                            a.CurrentInstallation.ToLower(CultureInfo.CurrentCulture).Equals(installationCode.ToLower(CultureInfo.CurrentCulture), StringComparison.Ordinal) &&
+                            a.CurrentArea != null);
+
+            foreach (var robot in robots)
+            {
+                _emergencyActionService.TriggerEmergencyButtonPressedForRobot(new EmergencyButtonPressedForRobotEventArgs(robot.Id, robot.CurrentArea!.Id));
+
+            }
+
+            return Ok("Request to abort current mission and move all robots back to safe position received");
+        }
+
+        /// <summary>
         ///     This query will clear the emergency state that is introduced by aborting the current mission and returning to a
         ///     safe zone. Clearing the emergency state means that mission runs that may be in the robots queue will start."
         /// </summary>
         [HttpPost]
-        [Route("{robotId}/{installationCode}/{areaName}/clear-emergency-state")]
+        [Route("{robotId}/{installationCode}/{areaName}/clear-robot-emergency-state")]
         [Authorize(Roles = Role.User)]
         [ProducesResponseType(typeof(MissionRun), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -97,6 +133,48 @@ namespace Api.Controllers
             }
 
             _emergencyActionService.TriggerEmergencyButtonDepressedForRobot(new EmergencyButtonPressedForRobotEventArgs(robot.Id, area.Id));
+
+            return Ok("Request to clear emergency state for robot was received");
+        }
+
+        /// <summary>
+        ///     This query will clear the emergency state that is introduced by aborting the current mission and returning to a
+        ///     safe zone. Clearing the emergency state means that mission runs that may be in the robots queue will start."
+        /// </summary>
+        [HttpPost]
+        [Route("{robotId}/{installationCode}/{areaName}/clear-emergency-state")]
+        [Authorize(Roles = Role.User)]
+        [ProducesResponseType(typeof(MissionRun), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> ClearInstallationEmergencyState(
+            [FromRoute] string robotId)
+        {
+            var robot = await _robotService.ReadById(robotId);
+
+            if (robot == null)
+            {
+                _logger.LogWarning("Could not find robot with id {Id}", robotId);
+                return NotFound("Robot not found");
+            }
+
+
+            if (robot.CurrentInstallation == null)
+            {
+                _logger.LogWarning("Could not find installation for robot with id {Id}", robotId);
+                return NotFound("Installation not found");
+            }
+
+            if (robot.CurrentArea == null)
+            {
+                _logger.LogWarning("Could not find area for robot with id {Id}", robotId);
+                return NotFound("Area not found");
+            }
+
+            _emergencyActionService.TriggerEmergencyButtonDepressedForRobot(new EmergencyButtonPressedForRobotEventArgs(robot.Id, robot.CurrentArea!.Id));
 
             return Ok("Request to clear emergency state for robot was received");
         }
