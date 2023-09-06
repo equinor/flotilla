5996b48f41b35763eb828165a8801745547b7869
oysand <outb@equinor.com>
2023-09-06 15:34:49 +0200

    Add DefaultLocalizationArea to deck

--- backend/api/Database/Models/Deck.cs
+++ backend/api/Database/Models/Deck.cs
@@ -14,6 +14,11 @@ namespace Api.Database.Models
 
         public virtual Installation Installation { get; set; }
 
+        public string? DefaultLocalizationAreaId { get; set; }
+
+        [ForeignKey("DefaultLocalizationAreaId")]
+        public virtual Area? DefaultLocalizationArea { get; set; }
+
         [Required]
         [MaxLength(200)]
         public string Name { get; set; }
