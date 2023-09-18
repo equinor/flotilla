6268ceb79392af7e2d951011f377c8e28686c773
Anders Chirico Indrebø <ACIN@equinor.com>
2023-09-18 15:28:14 +0200

    Remove redundant migration actions

--- backend/api/Migrations/20230830075517_RemoveInspectionFrequency.cs
+++ backend/api/Migrations/20230830075517_RemoveInspectionFrequency.cs
@@ -14,106 +14,16 @@ namespace Api.Migrations
             migrationBuilder.DropColumn(
                 name: "InspectionFrequency",
                 table: "MissionDefinitions");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "InstallationId",
-                table: "Plants",
-                type: "nvarchar(450)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "PlantId",
-                table: "Decks",
-                type: "nvarchar(450)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "InstallationId",
-                table: "Decks",
-                type: "nvarchar(450)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "PlantId",
-                table: "Areas",
-                type: "nvarchar(450)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "InstallationId",
-                table: "Areas",
-                type: "nvarchar(450)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)",
-                oldNullable: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<string>(
-                name: "InstallationId",
-                table: "Plants",
-                type: "nvarchar(450)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-
             migrationBuilder.AddColumn<TimeSpan>(
                 name: "InspectionFrequency",
                 table: "MissionDefinitions",
                 type: "time",
                 nullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "PlantId",
-                table: "Decks",
-                type: "nvarchar(450)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "InstallationId",
-                table: "Decks",
-                type: "nvarchar(450)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "PlantId",
-                table: "Areas",
-                type: "nvarchar(450)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "InstallationId",
-                table: "Areas",
-                type: "nvarchar(450)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
         }
     }
 }
