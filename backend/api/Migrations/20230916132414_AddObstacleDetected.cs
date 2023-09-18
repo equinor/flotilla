a1652dd7c92e755a88b9d067a480ab82055bd47b
UsamaEquinorAFK <muu@equinor.com>
2023-09-18 10:53:47 +0200

    Add migrations

---
+++ backend/api/Migrations/20230916132414_AddObstacleDetected.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddObstacleDetected : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ObstacleDectected",
+                table: "Robots",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ObstacleDectected",
+                table: "Robots");
+        }
+    }
+}
