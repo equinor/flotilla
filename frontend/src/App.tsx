c39e6286954a30382cf689b0f0dfba340f928b22
aestene <arnts@equinor.com>
2023-09-19 15:35:36 +0200

    Fix stop all robots button on the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,27 +5,30 @@ import { LanguageProvider } from 'components/Contexts/LanguageContext'
 import { MissionControlProvider } from 'components/Contexts/MissionControlContext'
 import { MissionFilterProvider } from 'components/Contexts/MissionFilterContext'
 import { MissionQueueProvider } from 'components/Contexts/MissionQueueContext'
+import { SafeZoneProvider } from 'components/Contexts/SafeZoneContext'
 
 function App() {
     return (
-        <MissionQueueProvider>
-            <LanguageProvider>
-                <MissionControlProvider>
-                    <>
-                        <UnauthenticatedTemplate>
-                            <div className="sign-in-page">
-                                <SignInPage></SignInPage>
-                            </div>
-                        </UnauthenticatedTemplate>
-                        <AuthenticatedTemplate>
-                            <MissionFilterProvider>
-                                <FlotillaSite />
-                            </MissionFilterProvider>
-                        </AuthenticatedTemplate>
-                    </>
-                </MissionControlProvider>
-            </LanguageProvider>
-        </MissionQueueProvider>
+        <SafeZoneProvider>
+            <MissionQueueProvider>
+                <LanguageProvider>
+                    <MissionControlProvider>
+                        <>
+                            <UnauthenticatedTemplate>
+                                <div className="sign-in-page">
+                                    <SignInPage></SignInPage>
+                                </div>
+                            </UnauthenticatedTemplate>
+                            <AuthenticatedTemplate>
+                                <MissionFilterProvider>
+                                    <FlotillaSite />
+                                </MissionFilterProvider>
+                            </AuthenticatedTemplate>
+                        </>
+                    </MissionControlProvider>
+                </LanguageProvider>
+            </MissionQueueProvider>
+        </SafeZoneProvider>
     )
 }
 
