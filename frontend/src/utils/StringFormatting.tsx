b2e9d875137242ce1e6f6102365d38775447cb13
aestene <arnts@equinor.com>
2023-09-26 12:19:37 +0200

    Handle the case where frequency or lastRun is null

--- frontend/src/utils/StringFormatting.tsx
+++ frontend/src/utils/StringFormatting.tsx
@@ -2,21 +2,23 @@ export const formatBackendDateTimeToDate = (date: Date) => {
     return new Date(date.toString())
 }
 
-export const getInspectionDeadline = (inspectionFrequency: string, lastRunTime: Date): Date => {
+export const getInspectionDeadline = (
+    inspectionFrequency: string | null,
+    lastRunTime: Date | null
+): Date | undefined => {
+    if (!inspectionFrequency || !lastRunTime) return undefined
+
     const dayHourSecondsArray = inspectionFrequency.split(':')
     const days: number = +dayHourSecondsArray[0]
     const hours: number = +dayHourSecondsArray[1]
     const minutes: number = +dayHourSecondsArray[2]
 
     lastRunTime = formatBackendDateTimeToDate(lastRunTime)
-
     let deadline = lastRunTime
     deadline.setDate(deadline.getDate() + days)
     deadline.setHours(deadline.getHours() + hours)
     deadline.setMinutes(deadline.getMinutes() + minutes)
     return deadline
-    // More flexibly we can also define the deadline in terms of milliseconds:
-    // new Date(lastRunTime.getTime() + (1000 * 60 * days) + (1000 * 60 * 60 * hours) + (1000 * 60 * 60 * 24 * days))
 }
 
 export const getDeadlineInDays = (deadlineDate: Date) => {
