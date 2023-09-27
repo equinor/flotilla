4d3d57b5024dece9f0d1c44c1dbd7cdc0520d0e3
aestene <arnts@equinor.com>
2023-09-27 15:00:19 +0200

    Refactor into dedicated map controller

---
+++ backend/api/Controllers/MapController.cs
@@ -0,0 +1,44 @@
+﻿using Api.Controllers.Models;
+using Api.Services;
+using Azure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("missions")]
+    public class MapController : ControllerBase
+    {
+        private readonly IMapService _mapService;
+        public MapController(IMapService mapService)
+        {
+            _mapService = mapService;
+        }
+
+        /// <summary>
+        ///     Get map for mission with specified id.
+        /// </summary>
+        [HttpGet]
+        [Authorize(Roles = Role.Any)]
+        [Route("{installationCode}/{mapName}/map")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status502BadGateway)]
+        public async Task<ActionResult<byte[]>> GetMap([FromRoute] string installationCode, string mapName)
+        {
+            try
+            {
+                byte[] mapStream = await _mapService.FetchMapImage(mapName, installationCode);
+                return File(mapStream, "image/png");
+            }
+            catch (RequestFailedException)
+            {
+                return NotFound("Could not find map for this area");
+            }
+        }
+    }
+}
--- backend/api/Controllers/MissionController.cs
+++ backend/api/Controllers/MissionController.cs
@@ -2,7 +2,6 @@
 using Api.Controllers.Models;
 using Api.Database.Models;
 using Api.Services;
-using Azure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 namespace Api.Controllers
@@ -45,32 +44,6 @@ namespace Api.Controllers
             _stidService = stidService;
             _sourceService = sourceService;
             _logger = logger;
-        }
-
-        /// <summary>
-        ///     Get map for mission with specified id.
-        /// </summary>
-        [HttpGet]
-        [Authorize(Roles = Role.Any)]
-        [Route("{installationCode}/{mapName}/map")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status502BadGateway)]
-        public async Task<ActionResult<byte[]>> GetMap([FromRoute] string installationCode, string mapName)
-        {
-            try
-            {
-                byte[] mapStream = await _mapService.FetchMapImage(mapName, installationCode);
-                return File(mapStream, "image/png");
-            }
-            catch (RequestFailedException)
-            {
-                return NotFound("Could not find map for this area.");
-            }
         }
 
         /// <summary>
