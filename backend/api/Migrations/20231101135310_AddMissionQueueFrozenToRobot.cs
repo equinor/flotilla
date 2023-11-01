79c44f6d45ebdcadb124be92d7f84c4584494b4b
Mariana R. Santos <mrica@equinor.com>
2023-11-01 14:59:35 +0100

    Add migrations for stop all robots

---
+++ backend/api/Migrations/20231101135310_AddMissionQueueFrozenToRobot.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissionQueueFrozenToRobot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MissionQueueFrozen",
+                table: "Robots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MissionRunPriority",
+                table: "MissionRuns",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MissionQueueFrozen",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "MissionRunPriority",
+                table: "MissionRuns");
+        }
+    }
+}
