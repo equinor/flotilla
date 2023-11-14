2208444c9cd6eaf2d73d0a90a2698fcade744bd9
Eddasol <esole@equinor.com>
2023-11-14 13:56:22 +0100

    Add DeckMapView

--- frontend/src/models/Deck.ts
+++ frontend/src/models/Deck.ts
@@ -1,6 +1,9 @@
+import { Pose } from './Pose'
+
 export interface Deck {
     id: string
     deckName: string
     plantName: string
     installationCode: string
+    defaultLocalizationPose?: Pose
 }
