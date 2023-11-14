7951ec7eed3ce01806f40a2029da1b1a0a2d86c9
Petter Rasmussen <PRASM@equinor.com>
2023-11-14 15:43:46 +0100

    Add missionRunResponse model to backend

---
+++ backend/api/Controllers/Models/MissionRunResponse.cs
@@ -0,0 +1,74 @@
+using Api.Database.Models;
+using System.Text.Json.Serialization;
+namespace Api.Controllers.Models
+{
+    public class MissionRunResponse
+    {
+        public string Id { get; set; }
+
+        public string? MissionId { get; set; }
+
+        public string? IsarMissionId { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? StatusReason { get; set; }
+
+        public string? Comment { get; set; }
+
+        public string InstallationCode { get; set; }
+
+        public AreaResponse? Area { get; set; }
+
+        public virtual RobotResponse Robot { get; set; }
+
+        public MissionStatus Status { get; set; }
+
+        public bool IsCompleted;
+
+        public DateTime DesiredStartTime { get; set; }
+
+        public DateTime? StartTime { get; private set; }
+
+        public DateTime? EndTime { get; private set; }
+
+        public uint? EstimatedDuration { get; set; }
+
+        public IList<MissionTask> Tasks { get; set; }
+
+        public MapMetadata? Map { get; set; }
+
+        public MissionRunPriority MissionRunPriority { get; set; }
+
+        [JsonConstructor]
+#nullable disable
+        public MissionRunResponse() { }
+#nullable enable
+
+        public MissionRunResponse(MissionRun mission)
+        {
+            Id = mission.Id;
+            MissionId = mission.MissionId;
+            IsarMissionId = mission.IsarMissionId;
+            Name = mission.Name;
+            Description = mission.Description;
+            StatusReason = mission.StatusReason;
+            Comment = mission.Comment;
+            InstallationCode = mission.InstallationCode;
+            Area = mission.Area != null ? new AreaResponse(mission.Area) : null;
+            Robot = new RobotResponse(mission.Robot);
+            Status = mission.Status;
+            IsCompleted = mission.IsCompleted;
+            DesiredStartTime = mission.DesiredStartTime;
+            StartTime = mission.StartTime;
+            EndTime = mission.EndTime;
+            EstimatedDuration = mission.EstimatedDuration;
+            Tasks = mission.Tasks;
+            Map = mission.Map;
+            MissionRunPriority = mission.MissionRunPriority;
+        }
+
+    }
+}
