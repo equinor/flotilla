3e3982fe3b81b26aed188bbc31be5b5693187076
UsamaEquinorAFK <muu@equinor.com>
2023-11-16 10:47:02 +0100

    Refactor inspection findings service

---
+++ backend/api/Migrations/20231116095346_RefactorInspectionFindingsTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorInspectionFindingsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "InspectionFinding");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InspectionId",
+                table: "InspectionFinding",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InspectionId",
+                table: "InspectionFinding",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Area",
+                table: "InspectionFinding",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
