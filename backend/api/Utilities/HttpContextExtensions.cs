965e637e95b0c35c865175623cba51f816bef798
Anders Chirico Indrebø <ACIN@equinor.com>
2023-11-22 13:07:12 +0100

    Add function to get user roles in controllers

---
+++ backend/api/Utilities/HttpContextExtensions.cs
@@ -0,0 +1,31 @@
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Api.Utilities
+{
+    public readonly record struct ExecutorIdentity(string ObjectId, string? ExecutorName);
+
+    public static class HttpContextExtensions
+    {
+        public static string GetRequestToken(this HttpContext client)
+        {
+            if (!client.Request.Headers.TryGetValue("Authorization", out var value))
+            {
+                throw new Exception("Not a protected endpoint!");
+            }
+
+            return value.ToString().Replace("Bearer ", "");
+        }
+
+        public static List<System.Security.Claims.Claim> GetRequestedRoles(this HttpContext client)
+        {
+            string accessTokenBase64 = client.GetRequestToken();
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = handler.ReadJwtToken(accessTokenBase64);
+
+            var claims = jwtSecurityToken.Claims;
+            var roles = claims.Where((c) => c.Type == "roles").ToList();
+            return roles;
+        }
+    }
+}
