db18e003d58c238c7a5be4dede1bf93c653ed8eb
Afonso <avie@equinor.com>
2023-12-08 15:51:43 +0100

    Test creating robot with a current area

--- backend/api.test/Client/RobotTests.cs
+++ backend/api.test/Client/RobotTests.cs
@@ -8,7 +8,9 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Api.Controllers.Models;
+using Api.Database.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 namespace Api.Test
 {
@@ -45,7 +47,7 @@ namespace Api.Test
             var response = await _client.GetAsync(url);
             var robots = await response.Content.ReadFromJsonAsync<List<RobotResponse>>(_serializerOptions);
             Assert.True(response.IsSuccessStatusCode);
-            Assert.True(robots != null && robots.Count == 3);
+            Assert.True(robots != null);
         }
 
         [Fact]
@@ -73,5 +75,112 @@ namespace Api.Test
             Assert.NotNull(robot);
             Assert.Equal(robot.Id, robotId);
         }
+
+        [Fact]
+        public async Task RobotIsCreatedWithCurrentArea()
+        {
+            // Area
+            string areaUrl = "/areas";
+            var areaResponse = await _client.GetAsync(areaUrl);
+            Assert.True(areaResponse.IsSuccessStatusCode);
+            var areas = await areaResponse.Content.ReadFromJsonAsync<List<AreaResponse>>(_serializerOptions);
+            Assert.True(areas != null);
+            var area = areas[0];
+
+            // Arrange - Create robot
+            var robotQuery = new CreateRobotQuery
+            {
+                IsarId = Guid.NewGuid().ToString(),
+                Name = "RobotGetNextRun",
+                SerialNumber = "GetNextRun",
+                RobotType = RobotType.Robot,
+                Status = RobotStatus.Available,
+                Enabled = true,
+                Host = "localhost",
+                Port = 3000,
+                CurrentInstallationCode = area.InstallationCode,
+                CurrentAreaName = area.AreaName,
+                VideoStreams = new List<CreateVideoStreamQuery>()
+            };
+
+            string robotUrl = "/robots";
+            var content = new StringContent(
+                JsonSerializer.Serialize(robotQuery),
+                null,
+                "application/json"
+            );
+            var response = await _client.PostAsync(robotUrl, content);
+            Assert.True(response != null, $"Failed to post to {robotUrl}. Null returned");
+            Assert.True(response.IsSuccessStatusCode, $"Failed to post to {robotUrl}. Status code: {response.StatusCode}");
+            var robot = await response.Content.ReadFromJsonAsync<RobotResponse>(_serializerOptions);
+            Assert.True(robot != null, $"No object returned from post to {robotUrl}");
+            Assert.True(robot.CurrentArea?.Id == area.Id);
+
+        }
+
+        [Fact]
+        public async Task RobotIsNotCreatedWithAreaNotInInstallation()
+        {
+            // Area
+            string areaUrl = "/areas";
+            var areaResponse = await _client.GetAsync(areaUrl);
+            Assert.True(areaResponse.IsSuccessStatusCode);
+            var areas = await areaResponse.Content.ReadFromJsonAsync<List<AreaResponse>>(_serializerOptions);
+            Assert.True(areas != null);
+            var area = areas[0];
+
+            // Installation
+            string testInstallation = "InstallationRobotIsNotCreatedWithAreaNotInInstallation";
+            var installationQuery = new CreateInstallationQuery
+            {
+                InstallationCode = testInstallation,
+                Name = testInstallation
+            };
+
+            var installationContent = new StringContent(
+                JsonSerializer.Serialize(installationQuery),
+                null,
+                "application/json"
+            );
+
+            string installationUrl = "/installations";
+            var installationResponse = await _client.PostAsync(installationUrl, installationContent);
+            Assert.True(installationResponse.IsSuccessStatusCode);
+            var wrongInstallation = await installationResponse.Content.ReadFromJsonAsync<Installation>(_serializerOptions);
+            Assert.True(wrongInstallation != null);
+
+            // Arrange - Create robot
+            var robotQuery = new CreateRobotQuery
+            {
+                IsarId = Guid.NewGuid().ToString(),
+                Name = "RobotGetNextRun",
+                SerialNumber = "GetNextRun",
+                RobotType = RobotType.Robot,
+                Status = RobotStatus.Available,
+                Enabled = true,
+                Host = "localhost",
+                Port = 3000,
+                CurrentInstallationCode = wrongInstallation.InstallationCode,
+                CurrentAreaName = area.AreaName,
+                VideoStreams = new List<CreateVideoStreamQuery>()
+            };
+
+            string robotUrl = "/robots";
+            var content = new StringContent(
+                JsonSerializer.Serialize(robotQuery),
+                null,
+                "application/json"
+            );
+
+            try
+            {
+                var response = await _client.PostAsync(robotUrl, content);
+            }
+            catch (DbUpdateException ex)
+            {
+                Assert.True(ex.Message == $"Could not create new robot in database as area '{area.AreaName}' does not exist in installation {wrongInstallation.InstallationCode}");
+            }
+        }
+
     }
 }
