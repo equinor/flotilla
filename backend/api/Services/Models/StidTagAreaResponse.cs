85ac6ad211b59cc807f3435d498e38fd1323bf92
oysand <outb@equinor.com>
2023-12-08 09:15:18 +0100

    Collect mission areas

---
+++ backend/api/Services/Models/StidTagAreaResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api.Services.Models
+{
+    public class StidTagAreaResponse
+    {
+        [JsonPropertyName("locationCode")]
+        public string? LocationCode { get; set; }
+
+    }
+}
