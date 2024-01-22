ba6a87ebb5d8718ac729afab250f69f7a4568599
Mariana R. Santos <mrica@equinor.com>
2024-01-22 11:16:06 +0100

    Add necessary changes to setup.sh file

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ echo -e "-----------------------------\n"
 
 #-------- BACKEND ------------
 echo "-------- BACKEND ------------"
-echo -e "Setting up backend ..."
+echo -e "Setting up backend .env root..."
 
 if [ -f $flotilla_dir/.env ]; then
     echo -e "WARNING: The file '$flotilla_dir/.env' already exists, it will be overwritten if the operation continues."
@@ -42,6 +42,7 @@ if [ -f $flotilla_dir/.env ]; then
         backend_abort="true"
     fi
 fi
+
 if [ "$backend_abort" != "true" ]; then
     echo -e "Flotilla azure client secret needed for backend dockerization."
     echo -en "Input Flotilla Azure Client Secret (copy-paste from KeyVault):\n" 
@@ -64,6 +65,101 @@ if [ "$backend_abort" != "true" ]; then
     dotnet user-secrets set "AzureAd:ClientSecret" $az_client_secret --project backend/api > /dev/null
     echo -e "Added client secret to ASP.NET secret manager"
 
+
+    echo -e "Flotilla broker server key needed for backend dockerization."
+    echo -en "Input Flotilla Broker Server Key (copy-paste from KeyVault):\n" 
+
+    while [ true ]
+    do
+        read -s broker_server_key
+
+        if [ -z "$broker_server_key" ]; then
+            echo "Flotilla broker server key cannot be empty"
+            echo "Try again:"
+        else
+            break
+        fi
+    done
+
+
+    echo "FLOTILLA_BROKER_SERVER_KEY='$broker_server_key'" >> $flotilla_dir/.env
+    echo -e "Added broker server key to .env file"
+
+fi
+
+#-------- BACKEND ------------
+echo "-------- BACKEND ------------"
+echo -e "Setting up backend .env /backend/api..."
+
+backend_abort="false"
+
+if [ -f $flotilla_dir/backend/api/.env ]; then
+    echo -e "WARNING: The file '$flotilla_dir/backend/api/.env' already exists, it will be overwritten if the operation continues."
+    echo -e "Is this ok? (Y/n)"
+
+    read reply
+    if [ "$reply" = "n" ] || [ "$reply" = "N" ]; then
+        echo -e "\nBackend setup - Aborted!\n"
+        backend_abort="true"
+    fi
+fi
+
+if [ "$backend_abort" != "true" ]; then
+
+    echo -e "Azure client id needed for backend dockerization."
+    echo -en "Azure Client Id (copy-paste from KeyVault):\n" 
+
+    while [ true ]
+    do
+        read -s az_client_id
+
+        if [ -z "$az_client_id" ]; then
+            echo "Azure client id cannot be empty"
+            echo "Try again:"
+        else
+            break
+        fi
+    done
+
+    echo "AZURE_CLIENT_ID='$az_client_id'" >> $flotilla_dir/backend/api/.env
+    echo -e "Added client id to .env file"
+
+    echo -e "Azure tenant id needed for backend dockerization."
+    echo -en "Azure Tenant Id (copy-paste from KeyVault):\n" 
+
+    while [ true ]
+    do
+        read -s az_tenant_id
+
+        if [ -z "$az_tenant_id" ]; then
+            echo "Azure tenant id cannot be empty"
+            echo "Try again:"
+        else
+            break
+        fi
+    done
+
+    echo "AZURE_TENANT_ID='$az_tenant_id'" >> $flotilla_dir/backend/api/.env
+    echo -e "Added tenant id to .env file"
+
+    echo -e "Azure client secret needed for backend dockerization."
+    echo -en "Azure Client Secret (copy-paste from KeyVault):\n" 
+
+    while [ true ]
+    do
+        read -s az_client_secret
+
+        if [ -z "$az_client_secret" ]; then
+            echo "Azure client secret cannot be empty"
+            echo "Try again:"
+        else
+            break
+        fi
+    done
+
+    echo "AZURE_CLIENT_SECRET='$az_client_secret'" >> $flotilla_dir/backend/api/.env
+    echo -e "Added client secret to .env file"
+    
     echo -e "A username is needed for local development with SignalR"
     echo -en "Input a username for yourself (this only needs to be unique within your development environment):\n" 
 
@@ -79,8 +175,7 @@ if [ "$backend_abort" != "true" ]; then
         fi
     done
 
-
-    echo "LOCAL_DEVUSERID='$local_dev_username'" >> $flotilla_dir/.env
+    echo "LOCAL_DEVUSERID='$local_dev_username'" >> $flotilla_dir/backend/api/.env
     echo -e "Added local development username to .env file"
 
     echo -e "Backup setup - Done!"
