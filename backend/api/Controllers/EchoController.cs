6151a4fb6db32111b82a330acfafc879c3f9840d
UsamaEquinorAFK <muu@equinor.com>
2024-02-05 12:29:52 +0100

    Fix mixed use of plantCode and installationCode

--- backend/api/Controllers/EchoController.cs
+++ backend/api/Controllers/EchoController.cs
@@ -19,18 +19,18 @@ namespace Api.Controllers
         ///     These missions are created in the Echo mission planner
         /// </remarks>
         [HttpGet]
-        [Route("available-missions/{plantCode}")]
+        [Route("available-missions/{installationCode}")]
         [ProducesResponseType(typeof(List<CondensedEchoMissionDefinition>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status502BadGateway)]
-        public async Task<ActionResult<IList<CondensedEchoMissionDefinition>>> GetAvailableEchoMissions([FromRoute] string? plantCode)
+        public async Task<ActionResult<IList<CondensedEchoMissionDefinition>>> GetAvailableEchoMissions([FromRoute] string? installationCode)
         {
             try
             {
-                var missions = await echoService.GetAvailableMissions(plantCode);
+                var missions = await echoService.GetAvailableMissions(installationCode);
                 return Ok(missions);
             }
             catch (HttpRequestException e)
