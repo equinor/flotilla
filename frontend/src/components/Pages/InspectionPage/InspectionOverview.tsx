3f0e888014c226e8f7267dede3e0197816421261
UsamaEquinorAFK <muu@equinor.com>
2024-02-05 15:38:10 +0100

    Remove lowercase from InstallationContext imports

--- frontend/src/components/Pages/InspectionPage/InspectionOverview.tsx
+++ frontend/src/components/Pages/InspectionPage/InspectionOverview.tsx
@@ -52,8 +52,8 @@ export const InspectionOverviewSection = () => {
     const [activeTab, setActiveTab] = useState(0)
 
     const isScheduleButtonDisabled =
-        enabledRobots.filter((r) => r.currentInstallation.installationCode === installationCode.toLocaleLowerCase())
-            .length === 0 || installationCode === ''
+        enabledRobots.filter((r) => r.currentInstallation.installationCode === installationCode).length === 0 ||
+        installationCode === ''
 
     const anchorRef = useRef<HTMLButtonElement>(null)
 
