31f6a34946b251003fa4c48115592b0108b329a5
Mariana R. Santos <mrica@equinor.com>
2024-04-09 16:12:57 +0200

    Fix frontend compile error

--- frontend/src/components/Pages/AssetSelectionPage/AssetSelectionPage.tsx
+++ frontend/src/components/Pages/AssetSelectionPage/AssetSelectionPage.tsx
@@ -119,6 +119,7 @@ const InstallationPicker = () => {
                 }}
             />
             <StyledCheckbox
+                crossOrigin={undefined}
                 label={TranslateText('Show only active installations')}
                 checked={showActivePlants}
                 onChange={(e) => setShowActivePlants(e.target.checked)}
