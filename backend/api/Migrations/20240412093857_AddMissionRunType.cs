0fc863c2561d7db331ce66593852379a87d76f2a
Mariana R. Santos <mrica@equinor.com>
2024-04-12 11:39:45 +0200

    Add migrations

---
+++ backend/api/Migrations/20240412093857_AddMissionRunType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissionRunType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MissionRunPriority",
+                table: "MissionRuns",
+                newName: "MissionRunType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MissionRunType",
+                table: "MissionRuns",
+                newName: "MissionRunPriority");
+        }
+    }
+}
