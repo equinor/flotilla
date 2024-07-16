b098c65943eec720dabab7ebf807144f3e129adb
Anders Chirico Indrebø <ACIN@equinor.com>
2024-07-16 15:29:40 +0200

    Add missing docking migrations

---
+++ backend/api/Migrations/20240716132916_AddDockToDefaultLocalisationPose.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDockToDefaultLocalisationPose : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DockingEnabled",
+                table: "DefaultLocalizationPoses",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DockingEnabled",
+                table: "DefaultLocalizationPoses");
+        }
+    }
+}
