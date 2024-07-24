4a6576c475a78bdcadc6d9e6ac2c03f4ddcb007b
Anders Chirico Indrebø <ACIN@equinor.com>
2024-07-24 12:23:21 +0200

    Add migrations for robot FlotillaStatus

---
+++ backend/api/Migrations/20240724102244_AddRobotFlotillaStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRobotFlotillaStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FlotillaStatus",
+                table: "Robots",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlotillaStatus",
+                table: "Robots");
+        }
+    }
+}
