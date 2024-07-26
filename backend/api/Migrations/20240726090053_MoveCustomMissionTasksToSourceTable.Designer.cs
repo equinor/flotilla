86ef1f4669a3af466a84702120f5d9c0789cff0b
Anders Chirico Indrebø <ACIN@equinor.com>
2024-07-26 11:01:26 +0200

    Add migrations for CustomMissionTasks

--- backend/api/Migrations/20240726090053_MoveCustomMissionTasksToSourceTable.Designer.cs
+++ backend/api/Migrations/20240726090053_MoveCustomMissionTasksToSourceTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Api.Database.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Api.Migrations
 {
     [DbContext(typeof(FlotillaDbContext))]
-    partial class FlotillaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240726090053_MoveCustomMissionTasksToSourceTable")]
+    partial class MoveCustomMissionTasksToSourceTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -453,10 +456,6 @@ namespace Api.Migrations
 
                     b.Property<bool>("Deprecated")
                         .HasColumnType("boolean");
-
-                    b.Property<string>("FlotillaStatus")
-                        .IsRequired()
-                        .HasColumnType("text");
 
                     b.Property<string>("Host")
                         .IsRequired()
---
+++ backend/api/Migrations/20240726090053_MoveCustomMissionTasksToSourceTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoveCustomMissionTasksToSourceTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomMissionTasks",
+                table: "Sources",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomMissionTasks",
+                table: "Sources");
+        }
+    }
+}
