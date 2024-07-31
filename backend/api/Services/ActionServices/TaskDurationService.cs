f2fee80335fed12373b9f20f21cfcbe48fed3791
Anders Chirico Indrebø <ACIN@equinor.com>
2024-07-31 13:38:45 +0200

    Add readonly capability to robotmodelservice

--- backend/api/Services/ActionServices/TaskDurationService.cs
+++ backend/api/Services/ActionServices/TaskDurationService.cs
@@ -26,7 +26,7 @@ namespace Api.Services.ActionServices
                 }
             );
 
-            var model = await robotModelService.ReadByRobotType(robotType);
+            var model = await robotModelService.ReadByRobotType(robotType, readOnly: true);
             if (model is null)
             {
                 logger.LogWarning("Could not update average duration for robot model {RobotType} as the model was not found", robotType);
