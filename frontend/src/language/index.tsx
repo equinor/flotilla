c85599a50c01f8b699a73011f176da3be89a8e15
Eddasol <esole@equinor.com>
2024-11-28 16:09:40 +0100

    Change default language to Norwegian

--- frontend/src/language/index.tsx
+++ frontend/src/language/index.tsx
@@ -1,6 +1,6 @@
 import en from './en.json'
 import no from './no.json'
 
-export const defaultLanguage = 'en'
+export const defaultLanguage = 'no'
 export const languageOptions: { [key: string]: string } = { en: 'English', no: 'Norsk' }
 export const allLanguageDictionaries: { [key: string]: Record<string, string> } = { en, no }
