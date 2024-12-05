0cc5a37e6683d3b79c63847e3c45742b66ad4bd0
Mariana R. Santos <mrica@equinor.com>
2024-12-05 14:39:37 +0100

    Fix white front page

--- frontend/src/components/Pages/FrontPage/FrontPage.tsx
+++ frontend/src/components/Pages/FrontPage/FrontPage.tsx
@@ -6,8 +6,8 @@ import { tokens } from '@equinor/eds-tokens'
 import { MissionControlSection } from './MissionOverview/MissionControlSection'
 
 const StyledFrontPage = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(100%, 1fr));
+    display: flex;
+    flex-direction: column;
     gap: 3rem;
     padding: 15px 15px;
     background-color: ${tokens.colors.ui.background__light.hex};
