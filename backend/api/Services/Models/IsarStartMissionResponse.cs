716ba89c4e523b2c65196bfe8e7c9118efe12d0b
Thomas Sundvoll <35451859+tsundvoll@users.noreply.github.com>
2024-12-12 14:44:48 +0100

    Set inspection id from isar response

--- backend/api/Services/Models/IsarStartMissionResponse.cs
+++ backend/api/Services/Models/IsarStartMissionResponse.cs
@@ -1,30 +1,31 @@
 ﻿using System.Text.Json.Serialization;
 
-#nullable disable
+
 namespace Api.Services.Models
 {
     public class IsarStartMissionResponse
     {
         [JsonPropertyName("id")]
-        public string MissionId { get; set; }
+        public required string MissionId { get; set; }
 
         [JsonPropertyName("tasks")]
-        public IList<IsarTaskResponse> Tasks { get; set; }
+        public required IList<IsarTaskResponse> Tasks { get; set; }
     }
 
     public class IsarTaskResponse
     {
         [JsonPropertyName("id")]
-        public string IsarTaskId { get; set; }
+        public required string IsarTaskId { get; set; }
 
         [JsonPropertyName("tag_id")]
-        public string TagId { get; set; }
+        public required string TagId { get; set; }
+
+        [JsonPropertyName("inspection_id")]
+        public string? IsarInspectionId { get; set; }
 
         [JsonPropertyName("type")]
-        public string TaskType { get; set; }
+        public required string TaskType { get; set; }
 
-        [JsonPropertyName("task_action_id")]
-        public string TaskActionId { get; set; }
 
     }
 }
