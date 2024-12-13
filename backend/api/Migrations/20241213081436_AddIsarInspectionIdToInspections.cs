53c26c5ac292784ca825a6a70f8f6209e3c8f640
Thomas Sundvoll <35451859+tsundvoll@users.noreply.github.com>
2024-12-13 09:18:02 +0100

    Add migration for IsarInspectionId to Inspection

---
+++ backend/api/Migrations/20241213081436_AddIsarInspectionIdToInspections.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsarInspectionIdToInspections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IsarInspectionId",
+                table: "Inspections",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsarInspectionId",
+                table: "Inspections");
+        }
+    }
+}
