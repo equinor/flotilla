f59d274aa83eb04c1428b7d02bf14a71d1954d12
Mariana R. Santos <mrica@equinor.com>
2024-12-17 16:53:46 +0100

    Revert call IDA using app

--- backend/api/Services/InspectionService.cs
+++ backend/api/Services/InspectionService.cs
@@ -114,7 +114,7 @@ namespace Api.Services
         {
             string relativePath = $"InspectionData/{inspectionId}/inspection-data-storage-location";
 
-            var response = await idaApi.CallApiForAppAsync(
+            var response = await idaApi.CallApiForUserAsync(
                 ServiceName,
                 options =>
                 {
