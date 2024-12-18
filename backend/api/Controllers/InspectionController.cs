2c04e684b64c3303ffd13a86561364524d8f16cd
Thomas Sundvoll <35451859+tsundvoll@users.noreply.github.com>
2024-12-18 12:49:38 +0100

    Simplify inspection controller
    by moving more functionality to the inspection service
    and avoiding a call to the Flotilla database
    by using the ISAR Inspection ID directly from the frontend
    to make the call to the IDA API

--- backend/api/Controllers/InspectionController.cs
+++ backend/api/Controllers/InspectionController.cs
@@ -1,9 +1,9 @@
-﻿using System.Globalization;
-using Api.Controllers.Models;
+﻿using Api.Controllers.Models;
 using Api.Database.Models;
 using Api.Services;
 using Api.Services.MissionLoaders;
 using Api.Services.Models;
+using Api.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -53,82 +53,42 @@ namespace Api.Controllers
         }
 
         /// <summary>
-        /// Lookup the inspection image for task with specified isarTaskId
+        /// Lookup the inspection image for task with specified isarInspectionId
         /// </summary>
         /// <remarks>
+        /// Retrieves the inspection image associated with the given ISAR Inspection ID.
         /// </remarks>
-        [HttpGet]
+        [HttpGet("{isarInspectionId}")]
         [Authorize(Roles = Role.User)]
-        [Route("{installationCode}/{taskId}/taskId")]
         [ProducesResponseType(typeof(Inspection), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status409Conflict)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Inspection>> GetInspectionImageById(
-            [FromRoute] string installationCode,
-            [FromRoute] string taskId
+        public async Task<ActionResult<Inspection>> GetInspectionImageByIsarInspectionId(
+            [FromRoute] string isarInspectionId
         )
         {
-            Inspection? inspection;
             try
             {
-                inspection = await inspectionService.ReadByIsarTaskId(taskId, readOnly: true);
-                if (inspection == null)
-                    return NotFound($"Could not find inspection for task with Id {taskId}.");
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, "Error while finding an inspection with {taskId}", taskId);
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-
-            if (inspection.IsarInspectionId == null)
-                return NotFound(
-                    $"Could not find isar inspection Id {inspection.IsarInspectionId} for Inspection with task ID {taskId}."
-                );
-
-            var inspectionData = await inspectionService.GetInspectionStorageInfo(
-                inspection.IsarInspectionId
-            );
-
-            if (inspectionData == null)
-                return NotFound(
-                    $"Could not find inspection data for inspection with isar Id {inspection.IsarInspectionId}."
-                );
-
-            if (
-                !inspectionData
-                    .BlobContainer.ToLower(CultureInfo.CurrentCulture)
-                    .Equals(
-                        installationCode.ToLower(CultureInfo.CurrentCulture),
-                        StringComparison.Ordinal
-                    )
-            )
-            {
-                return NotFound(
-                    $"Could not find inspection data for inspection with isar Id {inspection.IsarInspectionId} because blob name {inspectionData.BlobName} does not match installation {installationCode}."
-                );
-            }
-
-            try
-            {
-                byte[]? inspectionStream = await inspectionService.FetchInpectionImage(
-                    inspectionData.BlobName,
-                    inspectionData.BlobContainer,
-                    inspectionData.StorageAccount
-                );
+                byte[]? inspectionStream =
+                    await inspectionService.FetchInpectionImageFromIsarInspectionId(
+                        isarInspectionId
+                    );
 
                 if (inspectionStream == null)
-                    return NotFound($"Could not retrieve inspection with task Id {taskId}");
+                    return NotFound(
+                        $"Could not fetch inspection with ISAR Inspection ID {isarInspectionId}"
+                    );
 
                 return File(inspectionStream, "image/png");
             }
-            catch (Azure.RequestFailedException)
+            catch (InspectionNotFoundException e)
             {
+                logger.LogError(e, "{ErrorMessage}", e.Message);
                 return NotFound(
-                    $"Could not find inspection blob {inspectionData.BlobName} in container {inspectionData.BlobContainer} and storage account {inspectionData.StorageAccount}."
+                    $"Could not find inspection image with ISAR Inspection ID {isarInspectionId}"
                 );
             }
         }
