479b533975014ca67b13a35b1a20eebf24a52193
Anders Chirico Indrebø <ACIN@equinor.com>
2025-01-06 14:47:15 +0100

    Do partial robot updates with SignalR

---
+++ backend/api/Controllers/Models/RobotAttributeResponse.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+using Api.Database.Models;
+
+namespace Api.Controllers.Models
+{
+    public class RobotAttributeResponse
+    {
+        public string Id { get; set; }
+
+        public string PropertyName { get; set; }
+
+        public object? Value { get; set; }
+
+        [JsonConstructor]
+#nullable disable
+        public RobotAttributeResponse() { }
+
+#nullable enable
+
+        public RobotAttributeResponse(string robotId, string propertyName, object? robotProperty)
+        {
+            Id = robotId;
+            PropertyName = propertyName;
+            Value = robotProperty;
+            if (
+                !typeof(RobotResponse)
+                    .GetProperties()
+                    .Any(property => property.Name == propertyName)
+            )
+            {
+                throw new ArgumentException(
+                    $"Property {robotProperty} does not match any attributes in the RobotAttributeResponse class"
+                );
+            }
+        }
+    }
+}
--- frontend/src/components/Contexts/RobotContext.tsx
+++ frontend/src/components/Contexts/RobotContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, FC, useEffect } from 'react'
 import { BackendAPICaller } from 'api/ApiCaller'
-import { Robot } from 'models/Robot'
+import { Robot, RobotAttributeUpdate } from 'models/Robot'
 import { SignalREventLabels, useSignalRContext } from './SignalRContext'
 import { useLanguageContext } from './LanguageContext'
 import { AlertType, useAlertContext } from './AlertContext'
@@ -8,11 +8,18 @@ import { FailedRequestAlertContent, FailedRequestAlertListContent } from 'compon
 import { AlertCategory } from 'components/Alerts/AlertsBanner'
 import { useInstallationContext } from './InstallationContext'
 
-const upsertRobotList = (list: Robot[], mission: Robot) => {
+const upsertRobotList = (list: Robot[], robot: Robot) => {
     const newList = [...list]
-    const i = newList.findIndex((e) => e.id === mission.id)
-    if (i > -1) newList[i] = mission
-    else newList.push(mission)
+    const i = newList.findIndex((e) => e.id === robot.id)
+    if (i > -1) newList[i] = robot
+    else newList.push(robot)
+    return newList
+}
+
+const updateRobotInList = (list: Robot[], robotId: string, mapping: (old: Robot) => Robot) => {
+    const newList = [...list]
+    const i = newList.findIndex((e) => e.id === robotId)
+    if (i > -1) newList[i] = mapping(newList[i])
     return newList
 }
 
@@ -57,6 +64,26 @@ export const RobotProvider: FC<Props> = ({ children }) => {
                 setEnabledRobots((oldRobotList) => {
                     let oldRobotListCopy = [...oldRobotList]
                     oldRobotListCopy = upsertRobotList(oldRobotListCopy, updatedRobot)
+                    return [...oldRobotListCopy]
+                })
+            })
+            registerEvent(SignalREventLabels.robotAttributeUpdated, (username: string, message: string) => {
+                const updatedRobot: RobotAttributeUpdate = JSON.parse(message)
+                // The check below makes it so that it is not treated as null in the code.
+
+                const updatedProperty = updatedRobot.propertyName
+                if (!updatedProperty) return
+
+                const updatedValue = updatedRobot.value
+
+                const updateFunction = (oldRobot: Robot): Robot => {
+                    if (Object.keys(oldRobot).includes(updatedProperty))
+                        oldRobot = { ...oldRobot, [updatedProperty]: updatedValue }
+                    return oldRobot
+                }
+                setEnabledRobots((oldRobotList) => {
+                    let oldRobotListCopy = [...oldRobotList]
+                    oldRobotListCopy = updateRobotInList(oldRobotListCopy, updatedRobot.id, updateFunction)
                     return [...oldRobotListCopy]
                 })
             })
