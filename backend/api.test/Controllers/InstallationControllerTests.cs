87c03a4fd3c1672d9ddab14606594fd23c78ac1d
aestene <arnts@equinor.com>
2025-01-14 10:31:35 +0100

    Add endpoint test for installation controller

---
+++ backend/api.test/Controllers/InstallationControllerTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Api.Controllers.Models;
+using Api.Services;
+using Api.Test.Database;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Api.Test.Controllers
+{
+    public class InstallationControllerTests : IAsyncLifetime
+    {
+        public required HttpClient Client;
+
+        public required IInstallationService InstallationService;
+
+        public async Task InitializeAsync()
+        {
+            string databaseName = Guid.NewGuid().ToString();
+            (string connectionString, var connection) = await TestSetupHelpers.ConfigureDatabase(
+                databaseName
+            );
+            var factory = TestSetupHelpers.ConfigureWebApplicationFactory(databaseName);
+            var serviceProvider = TestSetupHelpers.ConfigureServiceProvider(factory);
+
+            Client = TestSetupHelpers.ConfigureHttpClient(factory);
+            InstallationService = serviceProvider.GetRequiredService<IInstallationService>();
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+
+        [Fact]
+        public async Task TestCreateInstallationEndpoint()
+        {
+            // Arrange
+            var query = new CreateInstallationQuery
+            {
+                InstallationCode = "inst",
+                Name = "Installation",
+            };
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(query),
+                null,
+                "application/json"
+            );
+
+            // Act
+            const string Url = "/installations";
+            var response = await Client.PostAsync(Url, content);
+
+            // Assert
+            var installation = await InstallationService.ReadByInstallationCode(
+                query.InstallationCode
+            );
+
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Equal(query.Name, installation!.Name);
+        }
+    }
+}
