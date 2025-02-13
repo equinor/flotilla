addfd23957906574ff5b1ef363a2dd2458034714
aestene <arnts@equinor.com>
2025-02-13 13:02:15 +0100

    Add test for system available for mission logic

---
+++ backend/api/Services/Helpers/MissionSchedulingHelpers.cs
@@ -0,0 +1,54 @@
+using Api.Database.Models;
+
+namespace Api.Services.Helpers;
+
+public static class MissionSchedulingHelpers
+{
+    public static bool TheSystemIsAvailableToRunAMission(
+        Robot robot,
+        MissionRun missionRun,
+        ILogger logger
+    )
+    {
+        if (
+            robot.MissionQueueFrozen
+            && !(missionRun.IsEmergencyMission() || missionRun.IsReturnHomeMission())
+        )
+        {
+            logger.LogInformation(
+                "Mission run {MissionRunId} was not started as the mission run queue for robot {RobotName} is frozen",
+                missionRun.Id,
+                robot.Name
+            );
+            return false;
+        }
+
+        if (robot.Status is not RobotStatus.Available)
+        {
+            logger.LogInformation(
+                "Mission run {MissionRunId} was not started as the robot is not available",
+                missionRun.Id
+            );
+            return false;
+        }
+        if (!robot.IsarConnected)
+        {
+            logger.LogWarning(
+                "Mission run {MissionRunId} was not started as the robots {RobotId} isar instance is disconnected",
+                missionRun.Id,
+                robot.Id
+            );
+            return false;
+        }
+        if (robot.Deprecated)
+        {
+            logger.LogWarning(
+                "Mission run {MissionRunId} was not started as the robot {RobotId} is deprecated",
+                missionRun.Id,
+                robot.Id
+            );
+            return false;
+        }
+        return true;
+    }
+}
