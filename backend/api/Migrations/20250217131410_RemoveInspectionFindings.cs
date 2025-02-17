51a2b79040f1b4ddd68d15b0699da53aa645d8c4
mrica-equinor <mrica@equinor.com>
2025-02-17 14:14:40 +0100

    Add migrations

---
+++ backend/api/Migrations/20250217131410_RemoveInspectionFindings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveInspectionFindings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InspectionFindings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InspectionFindings",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Finding = table.Column<string>(type: "text", nullable: false),
+                    InspectionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    InspectionId = table.Column<string>(type: "text", nullable: true),
+                    IsarTaskId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InspectionFindings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InspectionFindings_Inspections_InspectionId",
+                        column: x => x.InspectionId,
+                        principalTable: "Inspections",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InspectionFindings_InspectionId",
+                table: "InspectionFindings",
+                column: "InspectionId");
+        }
+    }
+}
