923cdac8705ff3180bda8bb07c312dd91ef848f0
mrica-equinor <mrica@equinor.com>
2025-03-04 12:23:59 +0100

    Run background service when pod restarts

--- backend/api/HostedServices/InspectionFrequencyHostedService.cs
+++ backend/api/HostedServices/InspectionFrequencyHostedService.cs
@@ -43,24 +43,30 @@ namespace Api.HostedServices
         {
             _logger.LogInformation("Inspection Frequency Hosted Service Running.");
 
-            var timeUntilMidnight = (DateTime.UtcNow.Date.AddDays(1) - DateTime.UtcNow).TotalSeconds;
+            var timeUntilMidnight = (
+                DateTime.UtcNow.Date.AddDays(1) - DateTime.UtcNow
+            ).TotalSeconds;
             _timer = new Timer(
                 DoWork,
                 null,
                 TimeSpan.FromSeconds(timeUntilMidnight),
                 TimeSpan.FromDays(1)
             );
+
+            DoWork(null);
             return Task.CompletedTask;
         }
 
         private async void DoWork(object? state)
         {
+
             var missionQuery = new MissionDefinitionQueryStringParameters();
 
             List<MissionDefinition>? missionDefinitions;
             try
             {
-                missionDefinitions = await MissionDefinitionService.ReadByHasAutoScheduleFrequency();
+                missionDefinitions =
+                    await MissionDefinitionService.ReadByHasAutoScheduleFrequency();
             }
             catch (InvalidDataException e)
             {
@@ -76,7 +82,7 @@ namespace Api.HostedServices
 
             var selectedMissionDefinitions = missionDefinitions.Where(m =>
                 m.AutoScheduleFrequency != null
-                && m.AutoScheduleFrequency.GetSchedulingTimesForNext24Hours() != null
+                && m.AutoScheduleFrequency.GetSchedulingTimesUntilMidnight() != null
             );
 
             if (selectedMissionDefinitions.Any() == false)
@@ -90,7 +96,7 @@ namespace Api.HostedServices
             foreach (var missionDefinition in selectedMissionDefinitions)
             {
                 var jobDelays =
-                    missionDefinition.AutoScheduleFrequency!.GetSchedulingTimesForNext24Hours();
+                    missionDefinition.AutoScheduleFrequency!.GetSchedulingTimesUntilMidnight();
 
                 if (jobDelays == null)
                 {
