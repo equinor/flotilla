a404ee9722e09bfa7dd40847ffd850f26eb9f35c
mrica-equinor <mrica@equinor.com>
2025-03-27 12:37:32 +0100

    Improve info page

--- frontend/src/components/Pages/InfoPage.tsx
+++ frontend/src/components/Pages/InfoPage.tsx
@@ -1,19 +1,78 @@
 import { Typography } from '@equinor/eds-core-react'
+import { tokens } from '@equinor/eds-tokens'
 import { useLanguageContext } from 'components/Contexts/LanguageContext'
 import { Header } from 'components/Header/Header'
 import { StyledPage } from 'components/Styles/StyledComponents'
 import styled from 'styled-components'
+import infoPageImage from 'mediaAssets/taurob-about-us.png'
 
-const StyledText = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-    max-width: 1000px;
+const StyledInfoPAge = styled(StyledPage)`
+    width: 100%;
+    margin-left: 0px;
+    padding-left: 0px;
 `
+
 const StyledParagraph = styled.div`
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
+`
+const StyledAboutSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    height: 250px;
+`
+
+const StyledTypography = styled(Typography)`
+    text-align: center;
+    width: 600px;
+    @media (max-width: 600px) {
+        width: 350px;
+    }
+`
+
+const StyledImage = styled.img`
+    max-heigth: 100%;
+    max-width: 45%;
+    padding-left: 20px;
+
+    @media (max-width: 600px) {
+        max-width: 100vw;
+        padding: 0px;
+    }
+`
+
+const StyledInfoSection = styled.div`
+    display: flex;
+    flex-direction: row;
+    background-color: ${tokens.colors.ui.background__default.rgba};
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: 500px) {
+        flex-direction: column;
+        gap: 2rem;
+        padding-top: 20px;
+    }
+`
+
+const StyledInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    gap: 2rem;
+
+    @media (max-width: 500px) {
+        width: 90%;
+    }
+`
+
+const JustifiedTypography = styled(Typography)`
+    text-align: justify;
 `
 
 export const InfoPage = () => {
@@ -22,22 +81,33 @@ export const InfoPage = () => {
     return (
         <>
             <Header page={'info'} />
-            <StyledPage>
-                <StyledText>
-                    <StyledParagraph>
-                        <Typography variant="h1">{TranslateText('Flotilla Info Page')}</Typography>
-                        <Typography variant="body_short">{TranslateText('Info: Flotilla is..')}</Typography>
-                    </StyledParagraph>
-                    <StyledParagraph>
-                        <Typography variant="h3">{TranslateText('Automatic scheduling of missions')}</Typography>
-                        <Typography variant="body_short">{TranslateText('Info: Autoscheduling..')}</Typography>
-                    </StyledParagraph>
-                    <StyledParagraph>
-                        <Typography variant="h5">{TranslateText('Updating mission tasks')}</Typography>
-                        <Typography variant="body_short">{TranslateText('Info: Updating mission tasks..')}</Typography>
-                    </StyledParagraph>
-                </StyledText>
-            </StyledPage>
+            <StyledInfoPAge>
+                <StyledAboutSection>
+                    <Typography variant="h1">{TranslateText('About Flotilla')}</Typography>
+                    <StyledTypography variant="body_short">{TranslateText('Info: Flotilla is..')}</StyledTypography>
+                </StyledAboutSection>
+                <StyledInfoSection>
+                    <StyledInfo>
+                        <StyledParagraph>
+                            <JustifiedTypography variant="h3">
+                                {TranslateText('Automatic scheduling of missions')}
+                            </JustifiedTypography>
+                            <JustifiedTypography variant="body_short">
+                                {TranslateText('Info: Autoscheduling..')}
+                            </JustifiedTypography>
+                        </StyledParagraph>
+                        <StyledParagraph>
+                            <JustifiedTypography variant="h4">
+                                {TranslateText('Updating mission tasks')}
+                            </JustifiedTypography>
+                            <JustifiedTypography variant="body_short">
+                                {TranslateText('Info: Updating mission tasks..')}
+                            </JustifiedTypography>
+                        </StyledParagraph>
+                    </StyledInfo>
+                    <StyledImage src={infoPageImage} />
+                </StyledInfoSection>
+            </StyledInfoPAge>
         </>
     )
 }
