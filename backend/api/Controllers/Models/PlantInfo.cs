f58dded28073dcb5740ae73fcbfb7e118efb8752
Eddasol <esole@equinor.com>
2025-04-04 13:20:49 +0200

    Add custom mission loader

--- backend/api/Controllers/Models/PlantInfo.cs
+++ backend/api/Controllers/Models/PlantInfo.cs
@@ -1,9 +1,25 @@
 ﻿#nullable disable
+using Api.Database.Models;
+
 namespace Api.Controllers.Models
 {
     public class PlantInfo
     {
         public string PlantCode { get; set; }
         public string ProjectDescription { get; set; }
+
+        public PlantInfo() { }
+
+        public PlantInfo(string plantCode, string projectDescription)
+        {
+            PlantCode = plantCode;
+            ProjectDescription = projectDescription;
+        }
+
+        public PlantInfo(Installation installation)
+        {
+            PlantCode = installation.InstallationCode;
+            ProjectDescription = installation.Name;
+        }
     }
 }
