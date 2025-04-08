d7ae6fd1f1c27bf493acb9f51e965fffbcb62863
Thomas Sundvoll <35451859+tsundvoll@users.noreply.github.com>
2025-04-08 15:27:30 +0200

    Remove unused AnalysisType
    this is now interpreted based on the InspectionDescription

---
+++ backend/api/Migrations/20250528125518_RemoveAnalysisType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAnalysisType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnalysisType",
+                table: "Inspections");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AnalysisType",
+                table: "Inspections",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
