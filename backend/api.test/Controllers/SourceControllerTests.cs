0b08ddfe713f0c3a6e97fee66d8e704732210b95
aestene <arnts@equinor.com>
2025-04-25 12:06:57 +0200

    Add test for retrieving source by ID

--- backend/api.test/Controllers/SourceControllerTests.cs
+++ backend/api.test/Controllers/SourceControllerTests.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Api.Controllers.Models;
 using Api.Database.Models;
 using Api.Services;
 using Api.Test.Database;
@@ -24,20 +23,17 @@ public class SourceControllerTests : IAsyncLifetime
 
     public async Task InitializeAsync()
     {
-        (Container, string connectionString, var connection) =
+        (Container, var connectionString, var connection) =
             await TestSetupHelpers.ConfigurePostgreSqlDatabase();
         var factory = TestSetupHelpers.ConfigureWebApplicationFactory(
             postgreSqlConnectionString: connectionString
         );
-        var serviceProvider = TestSetupHelpers.ConfigureServiceProvider(factory);
 
         Client = TestSetupHelpers.ConfigureHttpClient(factory);
         SerializerOptions = TestSetupHelpers.ConfigureJsonSerializerOptions();
         DatabaseUtilities = new DatabaseUtilities(
             TestSetupHelpers.ConfigurePostgreSqlContext(connectionString)
         );
-
-        SourceService = serviceProvider.GetRequiredService<ISourceService>();
     }
 
     public Task DisposeAsync() => Task.CompletedTask;
@@ -48,14 +44,25 @@ public class SourceControllerTests : IAsyncLifetime
         // Arrange
         var sourceOne = await DatabaseUtilities.NewSource(sourceId: "TestIdOne");
         var sourceTwo = await DatabaseUtilities.NewSource(sourceId: "TestIdTwo");
-        
+
         // Act
         var response = await Client.GetAsync($"/sources");
         var sources = await response.Content.ReadFromJsonAsync<List<Source>>(SerializerOptions);
-        
+
         // Assert
         Assert.Equal(2, sources!.Count);
         Assert.Equal(sourceOne.Id, sources[0].Id);
         Assert.Equal(sourceTwo.Id, sources[1].Id);
+    }
+
+    [Fact]
+    public async Task CheckThatLookupSourceByIdWorksAsExpected()
+    {
+        var source = await DatabaseUtilities.NewSource();
+        var response = await Client.GetAsync($"/sources/{source.Id}");
+        var sourceFromResponse = await response.Content.ReadFromJsonAsync<Source>(
+            SerializerOptions
+        );
+        Assert.Equal(source.Id, sourceFromResponse!.Id);
     }
 }
