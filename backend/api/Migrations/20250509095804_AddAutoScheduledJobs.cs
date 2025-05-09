2ad29d0c4f71f9b170cd5d7fc59de2e63a9eb60d
mrica-equinor <mrica@equinor.com>
2025-05-09 11:58:36 +0200

    Add migrations

---
+++ backend/api/Migrations/20250509095804_AddAutoScheduledJobs.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAutoScheduledJobs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AutoScheduleFrequency_AutoScheduledJobs",
+                table: "MissionDefinitions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutoScheduleFrequency_AutoScheduledJobs",
+                table: "MissionDefinitions");
+        }
+    }
+}
