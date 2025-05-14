5d67eb4154761dfcfefe8bdbbc47267cceb2bb95
Eddasol <esole@equinor.com>
2025-05-14 14:04:43 +0200

    Display completed and skipped missions as disabled

--- frontend/src/components/Pages/FrontPage/AutoScheduleSection/AutoScheduleSection.tsx
+++ frontend/src/components/Pages/FrontPage/AutoScheduleSection/AutoScheduleSection.tsx
@@ -5,7 +5,7 @@ import { StyledDialog, StyledTableBody, StyledTableCell, TextAlignedButton } fro
 import { DaysOfWeek, parseAutoScheduledJobIds } from 'models/AutoScheduleFrequency'
 import { config } from 'config'
 import styled from 'styled-components'
-import { capitalizeFirstLetter } from 'utils/StringFormatting'
+import { capitalizeFirstLetter, convertUTCDateToLocalDate } from 'utils/StringFormatting'
 import { Icons } from 'utils/icons'
 import { useState } from 'react'
 import { FormCard } from 'components/Pages/MissionDefinitionPage/MissionDefinitionStyledComponents'
@@ -90,28 +90,65 @@ const AutoScheduleMissionTableRow = ({
 }) => {
     const { TranslateText } = useLanguageContext()
 
+    enum MissionStatusType {
+        ScheduledJob = 'ScheduledJob',
+        SkippedJob = 'SkippedJob',
+        PastJob = 'PastJob',
+        FutureUnstartedJob = 'FutureUnstartedJob',
+    }
+
     const currentDayOfTheWeek = allDays[allDaysIndexOfToday]
+
+    const selectMissionStatusType = () => {
+        if (day !== currentDayOfTheWeek) {
+            return MissionStatusType.FutureUnstartedJob
+        }
+        if (time < convertUTCDateToLocalDate(new Date()).toISOString().substring(11, 19)) {
+            return MissionStatusType.PastJob
+        }
+        if (
+            mission.autoScheduleFrequency &&
+            mission.autoScheduleFrequency.autoScheduledJobs &&
+            parseAutoScheduledJobIds(mission.autoScheduleFrequency.autoScheduledJobs)[time]
+        ) {
+            return MissionStatusType.ScheduledJob
+        }
+        return MissionStatusType.SkippedJob
+    }
+
+    const missionStatusType = selectMissionStatusType()
+
+    const typographyColor =
+        missionStatusType === MissionStatusType.SkippedJob || missionStatusType === MissionStatusType.PastJob
+            ? 'disabled'
+            : 'primary'
 
     return (
         <Table.Row key={mission.id + time}>
             <Table.Cell>
                 <StyledTableRow>
-                    <Typography>{`${time.substring(0, 5)}`}</Typography>
-                    <Typography as={Link} to={`${config.FRONTEND_BASE_ROUTE}/mission-definition/${mission.id}`} link>
+                    <Typography color={typographyColor}>{`${time.substring(0, 5)}`}</Typography>
+                    <Typography
+                        color={typographyColor}
+                        as={Link}
+                        to={`${config.FRONTEND_BASE_ROUTE}/mission-definition/${mission.id}`}
+                        link
+                    >
                         {mission.name}
                     </Typography>
-                    {day === currentDayOfTheWeek &&
-                        mission.autoScheduleFrequency &&
-                        mission.autoScheduleFrequency.autoScheduledJobs &&
-                        parseAutoScheduledJobIds(mission.autoScheduleFrequency.autoScheduledJobs)[time] && (
-                            <Button
-                                style={{ maxWidth: '100px' }}
-                                variant="ghost"
-                                onClick={() => skipAutoScheduledMission(mission.id, time)}
-                            >
-                                {TranslateText('SkipAutoMission')}
-                            </Button>
-                        )}
+                    {(missionStatusType === MissionStatusType.ScheduledJob ||
+                        missionStatusType === MissionStatusType.SkippedJob) && (
+                        <Button
+                            style={{ maxWidth: '100px' }}
+                            variant="ghost"
+                            disabled={missionStatusType === MissionStatusType.SkippedJob}
+                            onClick={() => skipAutoScheduledMission(mission.id, time)}
+                        >
+                            {missionStatusType === MissionStatusType.ScheduledJob
+                                ? TranslateText('SkipAutoMission')
+                                : TranslateText('Skipped')}
+                        </Button>
+                    )}
                 </StyledTableRow>
             </Table.Cell>
         </Table.Row>
