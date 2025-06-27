ed9461431d52b2cc0801d8d97a1c060e007be296
Anders Chirico Indrebø <aindreboe@gmail.com>
2025-06-27 14:29:42 +0200

    Make inspectionArea polygon an owned property

---
+++ backend/api/Migrations/20250627122538_MakeAreaPolygonOwned.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeAreaPolygonOwned : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AreaPolygonJson",
+                table: "InspectionAreas",
+                newName: "AreaPolygon_Positions");
+
+            migrationBuilder.AddColumn<double>(
+                name: "AreaPolygon_ZMax",
+                table: "InspectionAreas",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "AreaPolygon_ZMin",
+                table: "InspectionAreas",
+                type: "double precision",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AreaPolygon_ZMax",
+                table: "InspectionAreas");
+
+            migrationBuilder.DropColumn(
+                name: "AreaPolygon_ZMin",
+                table: "InspectionAreas");
+
+            migrationBuilder.RenameColumn(
+                name: "AreaPolygon_Positions",
+                table: "InspectionAreas",
+                newName: "AreaPolygonJson");
+        }
+    }
+}
