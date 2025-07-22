43fe4c706ea4a50c77b2d9f5723d4ace8e69a972
mrica-equinor <mrica@equinor.com>
2025-07-22 10:24:42 +0200

    Add migrations

---
+++ backend/api/Migrations/20250722082429_RemoveInspectionStatus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveInspectionStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Inspections");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Inspections");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Inspections");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "Inspections",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "Inspections",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Inspections",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
