aa1f194884740beb6de0f61468b08c672a9a4442
Ola Alstad <OALST@equinor.com>
2025-08-12 14:04:17 +0200

    Add open telemetry flotilla backend

---
+++ backend/api/Configurations/OpenTelemetryConfiguration.cs
@@ -0,0 +1,83 @@
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+using Npgsql;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Api.Configurations;
+
+public static class TelemetryConfigurations
+{
+    public static WebApplicationBuilder AddCustomOpenTelemetry(
+        this WebApplicationBuilder builder,
+        ActivitySource activitySource,
+        Meter meter
+    )
+    {
+        // Logging
+        builder.Logging.AddOpenTelemetry(logging =>
+        {
+            logging.IncludeScopes = true;
+            logging.IncludeFormattedMessage = true;
+        });
+
+        // Tracing & Metrics pipeline
+        var otel = builder
+            .Services.AddOpenTelemetry()
+            .ConfigureResource(r => r.AddService("FlotillaBackend"))
+            .WithTracing(t =>
+            {
+                t.SetSampler(new AlwaysOnSampler())
+                    .SetResourceBuilder(
+                        ResourceBuilder
+                            .CreateDefault()
+                            .AddService("FlotillaBackend", serviceVersion: "0.0.1")
+                    )
+                    .AddAspNetCoreInstrumentation()
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddNpgsql()
+                    .AddSource(activitySource.Name);
+            })
+            .WithMetrics(m =>
+            {
+                m.AddAspNetCoreInstrumentation()
+                    .AddMeter(meter.Name)
+                    .AddMeter("Microsoft.AspNetCore.Hosting")
+                    .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+                    .AddRuntimeInstrumentation()
+                    .AddProcessInstrumentation();
+            });
+
+        // Connect to Azure Monitor if connection string is provided
+        var applicationInsightsConnectionString = builder.Configuration[
+            "ApplicationInsights:ConnectionString"
+        ];
+
+        if (!string.IsNullOrEmpty(applicationInsightsConnectionString))
+        {
+            builder
+                .Services.AddOpenTelemetry()
+                .UseAzureMonitor(o =>
+                {
+                    o.ConnectionString = applicationInsightsConnectionString;
+                });
+        }
+
+        // Connect to OpenTelemetry OTLP exporter if endpoint is provided, used for local aspire dashboard
+        var openTelemetryEndpoint = builder.Configuration["OpenTelemetry:OtelExporterOtlpEndpoint"];
+        if (!string.IsNullOrWhiteSpace(openTelemetryEndpoint))
+        {
+            var uri = new Uri(openTelemetryEndpoint);
+            var protocol = OtlpExportProtocol.Grpc;
+
+            otel.UseOtlpExporter(protocol, uri);
+        }
+
+        return builder;
+    }
+}
