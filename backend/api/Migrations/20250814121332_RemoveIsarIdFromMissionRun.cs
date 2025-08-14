fb76c118fa817946d5e2a85ed5ba204035bd4fbc
Ola Alstad <OALST@equinor.com>
2025-08-14 14:13:58 +0200

    Create migration remove isar id from mission run

---
+++ backend/api/Migrations/20250814121332_RemoveIsarIdFromMissionRun.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIsarIdFromMissionRun : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsarMissionId",
+                table: "MissionRuns");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IsarMissionId",
+                table: "MissionRuns",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+    }
+}
