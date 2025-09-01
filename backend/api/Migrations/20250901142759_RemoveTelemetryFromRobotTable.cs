65f07bbdcf2d84b869f673021f101359489fa871
Anders Chirico Indrebø <aindreboe@gmail.com>
2025-09-01 16:28:37 +0200

    Add migrations to remove battery/pressure

---
+++ backend/api/Migrations/20250901142759_RemoveTelemetryFromRobotTable.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTelemetryFromRobotTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BatteryLevel",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "BatteryState",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "Pose_Orientation_W",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "Pose_Orientation_X",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "Pose_Orientation_Y",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "Pose_Orientation_Z",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "Pose_Position_X",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "Pose_Position_Y",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "Pose_Position_Z",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "PressureLevel",
+                table: "Robots");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "BatteryLevel",
+                table: "Robots",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BatteryState",
+                table: "Robots",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Pose_Orientation_W",
+                table: "Robots",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Pose_Orientation_X",
+                table: "Robots",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Pose_Orientation_Y",
+                table: "Robots",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Pose_Orientation_Z",
+                table: "Robots",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Pose_Position_X",
+                table: "Robots",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Pose_Position_Y",
+                table: "Robots",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Pose_Position_Z",
+                table: "Robots",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "PressureLevel",
+                table: "Robots",
+                type: "real",
+                nullable: true);
+        }
+    }
+}
