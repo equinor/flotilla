9de7ab9a760516e82b55ead02741629f389ccbc5
akust <211008703+akustn@users.noreply.github.com>
2025-09-12 11:45:24 +0200

    Remove unused exceptions

--- backend/api/Utilities/Exceptions.cs
+++ backend/api/Utilities/Exceptions.cs
@@ -72,15 +72,13 @@
         public int IsarStatusCode { get; set; }
     }
 
-    public class MissionSourceTypeException(string message) : Exception(message) { }
+
 
     public class SourceException(string message) : Exception(message) { }
 
     public class InstallationNotFoundException(string message) : Exception(message) { }
 
     public class PlantNotFoundException(string message) : Exception(message) { }
-
-    public class InspectionAreaNotFoundException(string message) : Exception(message) { }
 
     public class MissionNotFoundException(string message) : Exception(message) { }
 
@@ -92,40 +90,27 @@
 
     public class MissionRunNotFoundException(string message) : Exception(message) { }
 
-    public class RobotPositionNotFoundException(string message) : Exception(message) { }
-
     public class RobotNotFoundException(string message) : Exception(message) { }
 
     public class RobotInformationNotAvailableException(string message) : Exception(message) { }
 
     public class RobotPreCheckFailedException(string message) : Exception(message) { }
 
-    public class TagPositionNotFoundException(string message) : Exception(message) { }
-
-    public class AreaExistsException(string message) : Exception(message) { }
-
     public class InspectionAreaExistsException(string message) : Exception(message) { }
 
     public class ExclusionAreaExistsException(string message) : Exception(message) { }
-
-    public class DockException(string message) : Exception(message) { }
 
     public class RobotNotAvailableException(string message) : Exception(message) { }
 
     public class RobotBusyException(string message) : Exception(message) { }
 
     public class RobotNotInSameInstallationAsMissionException(string message)
-        : Exception(message) { }
-
-    public class PoseNotFoundException(string message) : Exception(message) { }
+        : Exception(message)
+    { }
 
     public class IsarCommunicationException(string message) : Exception(message) { }
 
-    public class RobotCurrentAreaMissingException(string message) : Exception(message) { }
-
     public class UnsupportedRobotCapabilityException(string message) : Exception(message) { }
-
-    public class DatabaseUpdateException(string message) : Exception(message) { }
 
     public class InvalidPolygonException(string message) : Exception(message) { }
 
