7a366e1e8accd9455cab3e2ca66787dcb88495ca
Thomas Sundvoll <35451859+tsundvoll@users.noreply.github.com>
2025-09-19 13:20:09 +0200

    Reintroduce https for swagger

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -176,7 +176,11 @@ app.UseSwagger(c =>
     c.PreSerializeFilters.Add(
         (swaggerDoc, httpReq) =>
         {
-            swaggerDoc.Servers = [new() { Url = $"http://{httpReq.Host.Value}{basePath}" }];
+            swaggerDoc.Servers =
+            [
+                new() { Url = $"https://{httpReq.Host.Value}{basePath}" },
+                new() { Url = $"http://{httpReq.Host.Value}{basePath}" },
+            ];
         }
     );
 });
