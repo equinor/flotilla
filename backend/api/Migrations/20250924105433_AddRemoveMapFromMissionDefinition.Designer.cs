c3090f615f153c4a7a5d02ca8304922f78580c3e
mrica-equinor <mrica@equinor.com>
2025-09-24 12:54:42 +0200

    Add migrations

--- backend/api/Migrations/20250924105433_AddRemoveMapFromMissionDefinition.Designer.cs
+++ backend/api/Migrations/20250924105433_AddRemoveMapFromMissionDefinition.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Api.Database.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Api.Migrations
 {
     [DbContext(typeof(FlotillaDbContext))]
-    partial class FlotillaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250924105433_AddRemoveMapFromMissionDefinition")]
+    partial class AddRemoveMapFromMissionDefinition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/api/Migrations/20250924105433_AddRemoveMapFromMissionDefinition.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRemoveMapFromMissionDefinition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Map_Boundary_X1",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_Boundary_X2",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_Boundary_Y1",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_Boundary_Y2",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_Boundary_Z1",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_Boundary_Z2",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_MapName",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_TransformationMatrices_C1",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_TransformationMatrices_C2",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_TransformationMatrices_D1",
+                table: "MissionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Map_TransformationMatrices_D2",
+                table: "MissionDefinitions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Map_Boundary_X1",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_Boundary_X2",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_Boundary_Y1",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_Boundary_Y2",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_Boundary_Z1",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_Boundary_Z2",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Map_MapName",
+                table: "MissionDefinitions",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_TransformationMatrices_C1",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_TransformationMatrices_C2",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_TransformationMatrices_D1",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Map_TransformationMatrices_D2",
+                table: "MissionDefinitions",
+                type: "double precision",
+                nullable: true);
+        }
+    }
+}
