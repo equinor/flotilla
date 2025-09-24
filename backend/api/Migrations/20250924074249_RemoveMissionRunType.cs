a586b98c8c459d78dcfe84b12440878a56d19850
andchiind <aindreboe@gmail.com>
2025-09-24 09:46:35 +0200

    Add migrations for removing missionruntype

---
+++ backend/api/Migrations/20250924074249_RemoveMissionRunType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMissionRunType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MissionRunType",
+                table: "MissionRuns");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MissionRunType",
+                table: "MissionRuns",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
