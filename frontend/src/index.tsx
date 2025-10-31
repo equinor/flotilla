68041fdac2ba930e377ac20d88ebc4ade4cb24f0
Christian de Jonge <chjo@equinor.com>
2025-10-31 09:54:59 +0100

    Fix msal redirect promise

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,9 +11,20 @@ const msalInstance = new PublicClientApplication(msalConfig)
 
 // Error handling for redirect login
 await msalInstance.initialize()
-msalInstance.handleRedirectPromise().catch((error) => {
-    console.error(error)
-})
+
+msalInstance
+    .handleRedirectPromise()
+    .then((tokenResponse) => {
+        if (!tokenResponse) {
+            const accounts = msalInstance.getAllAccounts()
+            if (accounts.length === 0) {
+                msalInstance.loginRedirect()
+            }
+        }
+    })
+    .catch((err) => {
+        console.error(err)
+    })
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
