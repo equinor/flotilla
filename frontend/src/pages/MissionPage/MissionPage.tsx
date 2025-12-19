daa76f30d7f60c20bc63346d39cf1e15e1f14901
Anders Chirico Indrebø <ACIN@equinor.com>
2025-12-19 12:56:18 +0100

    Add simple mission view

--- frontend/src/pages/MissionPage/MissionPage.tsx
+++ frontend/src/pages/MissionPage/MissionPage.tsx
@@ -2,7 +2,7 @@ import { VideoStreamWindow } from 'pages/MissionPage/VideoStream/VideoStreamWind
 import { Mission } from 'models/Mission'
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { MissionHeader } from './MissionHeader/MissionHeader'
+import { MissionHeader, SimpleMissionHeader } from './MissionHeader/MissionHeader'
 import { BackButton } from 'utils/BackButton'
 import { BackendAPICaller } from 'api/ApiCaller'
 import { Header } from 'components/Header/Header'
@@ -118,5 +118,77 @@ export const MissionPage = ({ missionId }: { missionId: string }) => {
                 )}
             </StyledPage>
         </>
+    )
+}
+
+export const SimpleMissionPage = ({ missionId }: { missionId: string }) => {
+    const { TranslateText } = useLanguageContext()
+    const { setAlert } = useAlertContext()
+    const [videoMediaStreams, setVideoMediaStreams] = useState<MediaStreamTrack[]>([])
+    const [selectedMission, setSelectedMission] = useState<Mission>()
+    const { registerEvent, connectionReady } = useSignalRContext()
+    const { mediaStreams, addMediaStreamConfigIfItDoesNotExist } = useMediaStreamContext()
+    const { selectedInspectionTask } = useInspectionsContext()
+
+    useEffect(() => {
+        if (selectedMission && !Object.keys(mediaStreams).includes(selectedMission?.robot.id))
+            addMediaStreamConfigIfItDoesNotExist(selectedMission?.robot.id)
+    }, [selectedMission])
+
+    useEffect(() => {
+        if (connectionReady) {
+            registerEvent(SignalREventLabels.missionRunUpdated, (username: string, message: string) => {
+                const updatedMission: Mission = JSON.parse(message)
+                setSelectedMission((oldMission) => (updatedMission.id === oldMission?.id ? updatedMission : oldMission))
+            })
+        }
+    }, [connectionReady])
+
+    useEffect(() => {
+        if (selectedMission && mediaStreams && Object.keys(mediaStreams).includes(selectedMission?.robot.id)) {
+            const mediaStreamConfig = mediaStreams[selectedMission?.robot.id]
+            if (mediaStreamConfig && mediaStreamConfig.streams.length > 0)
+                setVideoMediaStreams(mediaStreamConfig.streams)
+        }
+    }, [selectedMission, mediaStreams])
+
+    useEffect(() => {
+        if (missionId)
+            BackendAPICaller.getMissionRunById(missionId)
+                .then((mission) => {
+                    setSelectedMission(mission)
+                })
+                .catch(() => {
+                    setAlert(
+                        AlertType.RequestFail,
+                        <FailedRequestAlertContent
+                            translatedMessage={TranslateText('Failed to find mission with ID {0}', [missionId])}
+                        />,
+                        AlertCategory.ERROR
+                    )
+                })
+    }, [missionId])
+
+    return (
+        <StyledPage>
+            <BackButton />
+            {selectedMission !== undefined && (
+                <StyledMissionPageContent>
+                    <StyledCardsWidth>
+                        <SimpleMissionHeader mission={selectedMission} />
+                        <TaskTableAndMap mission={selectedMission} missionDefinitionPage={false} />
+                        <VideoStreamSection>
+                            {videoMediaStreams && videoMediaStreams.length > 0 && (
+                                <VideoStreamWindow videoStreams={videoMediaStreams} />
+                            )}
+                        </VideoStreamSection>
+                        {selectedInspectionTask && selectedInspectionTask.id && (
+                            <InspectionDialogView selectedTask={selectedInspectionTask} tasks={selectedMission.tasks} />
+                        )}
+                        <InspectionOverviewSection tasks={selectedMission.tasks} />
+                    </StyledCardsWidth>
+                </StyledMissionPageContent>
+            )}
+        </StyledPage>
     )
 }
