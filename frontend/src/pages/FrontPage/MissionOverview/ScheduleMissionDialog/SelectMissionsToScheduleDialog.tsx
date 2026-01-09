2fbb68f4f24d5ab099df4bafb98e8081ceba930b
Eddasol <esole@equinor.com>
2026-01-09 14:31:26 +0100

    Fix frontend build

--- frontend/src/pages/FrontPage/MissionOverview/ScheduleMissionDialog/SelectMissionsToScheduleDialog.tsx
+++ frontend/src/pages/FrontPage/MissionOverview/ScheduleMissionDialog/SelectMissionsToScheduleDialog.tsx
@@ -166,7 +166,7 @@ const SelectRobotComponent = memo(
 
         return (
             <Autocomplete
-                optionLabel={(r) => (r ? r.name + ' (' + r.model.type + ')' : '')}
+                optionLabel={(r: Robot | undefined) => (r ? r.name + ' (' + r.model.type + ')' : '')}
                 options={enabledRobots.filter(
                     (r) =>
                         (r.status === RobotStatus.Available ||
