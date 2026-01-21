05bacef06c33f002242bcf06eac8b6fb73c03b54
andchiind <aindreboe@gmail.com>
2026-01-21 11:28:31 +0100

    Add migration for disconnect time

---
+++ backend/api/Migrations/20260121102735_AddDisconnectTimeToRobotModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisconnectTimeToRobotModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DisconnectTime",
+                table: "Robots",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisconnectTime",
+                table: "Robots");
+        }
+    }
+}
