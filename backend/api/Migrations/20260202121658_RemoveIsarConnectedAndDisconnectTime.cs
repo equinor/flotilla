289fb3c213f999010239cf3d3b33c665e6a79d2a
andchiind <aindreboe@gmail.com>
2026-02-02 13:17:17 +0100

    Add migrations for removing isar connected

---
+++ backend/api/Migrations/20260202121658_RemoveIsarConnectedAndDisconnectTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIsarConnectedAndDisconnectTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisconnectTime",
+                table: "Robots");
+
+            migrationBuilder.DropColumn(
+                name: "IsarConnected",
+                table: "Robots");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DisconnectTime",
+                table: "Robots",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsarConnected",
+                table: "Robots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
