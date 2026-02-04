ba9ec0ddb0c301e158ee8fde2f53d363d319a8ef
andchiind <aindreboe@gmail.com>
2026-02-04 15:05:07 +0100

    Simplify PageRouter

--- frontend/src/pages/PageRouter.tsx
+++ frontend/src/pages/PageRouter.tsx
@@ -3,100 +3,73 @@ import { MissionPage, SimpleMissionPage } from './MissionPage/MissionPage'
 import { RobotPage } from './RobotPage/RobotPage'
 import { MissionDefinitionPage } from './MissionDefinitionPage/MissionDefinitionPage'
 import { config } from 'config'
-import { useEffect, useState } from 'react'
 import { useAssetContext } from 'components/Contexts/AssetContext'
 
-enum PageRouterPrefixes {
-    Mission = 'mission',
-    MissionDefinition = 'missiondefinition',
-    Robot = 'robot',
-    SimpleMission = 'mission-simple',
-}
-
-interface PageRouterProps {
-    prefix: PageRouterPrefixes
-}
-
-const PageRouter = ({ prefix }: PageRouterProps) => {
+export const SimpleMissionPageRouter = () => {
     const navigate = useNavigate()
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [searchParams, setSearchParams] = useSearchParams()
-    const [id, setId] = useState<string | undefined>(undefined)
-    const [inspectionId, setInspectionId] = useState<string | undefined>(undefined)
-    const [analysisId, setAnalysisId] = useState<string | undefined>(undefined)
-    const { enabledRobots } = useAssetContext()
-    const selectedRobot = enabledRobots.find((robot) => robot.id === id)
 
-    useEffect(() => {
-        if (!searchParams || searchParams.size < 1) {
-            navigate(`${config.FRONTEND_BASE_ROUTE}/page-not-found`)
-            return
-        }
+    const id = searchParams.get('id') ?? undefined
+    const inspectionId = searchParams.get('inspectionId') ?? undefined
+    const analysisId = searchParams.get('analysisId') ?? undefined
 
-        const id = searchParams.get('id')
-        const inspectionId = searchParams.get('inspectionId')
-        const analysisId = searchParams.get('analysisId')
-        if (!id || !ValidateUUID(id)) {
-            navigate(`${config.FRONTEND_BASE_ROUTE}/page-not-found`)
-            return
-        }
-        if (inspectionId) {
-            if (!ValidateUUID(inspectionId)) {
-                navigate(`${config.FRONTEND_BASE_ROUTE}/page-not-found`)
-                return
-            }
-            setInspectionId(inspectionId)
-        } else {
-            setInspectionId(undefined)
-        }
-        if (analysisId) {
-            if (!ValidateUUID(analysisId)) {
-                navigate(`${config.FRONTEND_BASE_ROUTE}/page-not-found`)
-                return
-            }
-            setAnalysisId(analysisId)
-        } else {
-            setAnalysisId(undefined)
-        }
-        setId(id)
-    }, [searchParams])
-
-    if (!id) return <></>
-
-    switch (prefix) {
-        case PageRouterPrefixes.SimpleMission:
-            return <SimpleMissionPage missionId={id} inspectionId={inspectionId} analysisId={analysisId} />
-        case PageRouterPrefixes.Mission:
-            return <MissionPage missionId={id} inspectionId={inspectionId} analysisId={analysisId} />
-        case PageRouterPrefixes.MissionDefinition:
-            return <MissionDefinitionPage missionId={id} />
-        case PageRouterPrefixes.Robot:
-            if (!selectedRobot) {
-                return <>Loading robot...</>
-            }
-            return <RobotPage robot={selectedRobot} />
-        default:
-            return <></>
+    if (!id) {
+        navigate(`${config.FRONTEND_BASE_ROUTE}/page-not-found`)
+        return <></>
     }
-}
 
-export const SimpleMissionPageRouter = () => {
-    return <PageRouter prefix={PageRouterPrefixes.SimpleMission} />
+    return <SimpleMissionPage missionId={id} inspectionId={inspectionId} analysisId={analysisId} />
 }
 
 export const MissionPageRouter = () => {
-    return <PageRouter prefix={PageRouterPrefixes.Mission} />
+    const navigate = useNavigate()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const id = searchParams.get('id') ?? undefined
+    const inspectionId = searchParams.get('inspectionId') ?? undefined
+    const analysisId = searchParams.get('analysisId') ?? undefined
+
+    if (!id) {
+        navigate(`${config.FRONTEND_BASE_ROUTE}/page-not-found`)
+        return <></>
+    }
+
+    return <MissionPage missionId={id} inspectionId={inspectionId} analysisId={analysisId} />
 }
 
 export const MissionDefinitionPageRouter = () => {
-    return <PageRouter prefix={PageRouterPrefixes.MissionDefinition} />
+    const navigate = useNavigate()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const id = searchParams.get('id') ?? undefined
+
+    if (!id) {
+        navigate(`${config.FRONTEND_BASE_ROUTE}/page-not-found`)
+        return <></>
+    }
+
+    return <MissionDefinitionPage missionId={id} />
 }
 
 export const RobotPageRouter = () => {
-    return <PageRouter prefix={PageRouterPrefixes.Robot} />
-}
+    const navigate = useNavigate()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [searchParams, setSearchParams] = useSearchParams()
+    const { enabledRobots } = useAssetContext()
 
-const ValidateUUID = (id: string) => {
-    const regex = /^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$/i
-    return regex.test(id)
+    const id = searchParams.get('id') ?? undefined
+    const selectedRobot = enabledRobots.find((robot) => robot.id === id)
+
+    if (!id) {
+        navigate(`${config.FRONTEND_BASE_ROUTE}/page-not-found`)
+        return <></>
+    }
+
+    if (!selectedRobot) {
+        return <>Loading robot...</>
+    }
+    return <RobotPage robot={selectedRobot} />
 }
