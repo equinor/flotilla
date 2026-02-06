9bba59503652475c3869264acd501d31a8b8f058
andchiind <aindreboe@gmail.com>
2026-02-06 12:19:03 +0100

    Add migration for IsDeprecated inspectionarea

---
+++ backend/api/Migrations/20260206111830_AddIsDeprecatedToInspectionArea.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeprecatedToInspectionArea : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeprecated",
+                table: "InspectionAreas",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeprecated",
+                table: "InspectionAreas");
+        }
+    }
+}
