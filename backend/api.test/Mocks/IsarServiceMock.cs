a74c93c0af0f42ce80df522ed1ffadb377061415
Nicholas Dalhaug <nicd@equinor.com>
2026-02-18 12:52:46 +0100

    Test that isar start called

--- backend/api.test/Mocks/IsarServiceMock.cs
+++ backend/api.test/Mocks/IsarServiceMock.cs
@@ -1,16 +1,41 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Api.Database.Models;
 using Api.Services;
 using Api.Services.Models;
+using Api.Utilities;
 
 namespace Api.Test.Mocks
 {
     public class MockIsarService : IIsarService
     {
+        public bool isStartCalled = false;
+        public bool isStarted = false;
+
         public async Task<IsarMission> StartMission(Robot robot, MissionRun mission)
         {
+            isStartCalled = true;
+            var startMissionStatuses = new List<RobotStatus>
+            {
+                RobotStatus.Available,
+                RobotStatus.Home,
+                RobotStatus.ReturnHomePaused,
+                RobotStatus.ReturningHome,
+            };
+            if (!startMissionStatuses.Contains(robot.Status))
+            {
+                if (robot.Status == RobotStatus.Busy)
+                    throw new RobotBusyException("Robot was not available when starting mission");
+                else
+                    throw new MissionException(
+                        $"Robot had the wrong status when starting mission. Robot status: ${robot.Status}"
+                    );
+            }
+
             await Task.Run(() => Thread.Sleep(1));
+            isStarted = true;
+
             var isarServiceMissionResponse = new IsarMission(
                 new IsarStartMissionResponse
                 {
