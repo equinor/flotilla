85c50817a7f92359d3a7c760e7b9b12cef3d2923
Nicholas Dalhaug <nicd@equinor.com>
2026-02-23 15:25:14 +0100

    Remove unused file

--- backend/api/MQTT/Events/MqttReceivedArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using Api.Mqtt.MessageModels;
-
-namespace Api.Mqtt.Events
-{
-    public class MqttReceivedArgs(MqttMessage message) : EventArgs
-    {
-        public MqttMessage Message { get; } = message;
-    }
-}
