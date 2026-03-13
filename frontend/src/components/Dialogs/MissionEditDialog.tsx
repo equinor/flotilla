e25ffe5b8bebb5a94f9dae92696354094cdaca38
Eddasol <esole@equinor.com>
2026-03-13 08:57:58 +0100

    Allow user to select no times in edit auto schedule

--- frontend/src/components/Dialogs/MissionEditDialog.tsx
+++ frontend/src/components/Dialogs/MissionEditDialog.tsx
@@ -147,12 +147,7 @@ export const MissionSchedulingEditDialog = ({ mission, isOpen, onClose }: Missio
                         <Button onClick={onClose} variant="outlined" color="primary">
                             {TranslateText('Cancel')}
                         </Button>
-                        <Button
-                            onClick={onSubmit}
-                            disabled={schedulingTimes.length === 0}
-                            variant="contained"
-                            color="primary"
-                        >
+                        <Button onClick={onSubmit} variant="contained" color="primary">
                             {TranslateText('Update')}
                         </Button>
                     </ButtonSection>
@@ -375,7 +370,7 @@ const SelectedTimeDaySummary = ({
         <StyledSummary>
             <Typography variant="h6">{TranslateText('Selected times')}</Typography>
             {schedulingTimes.length === 0 && (
-                <Typography color="warning">{TranslateText('No times have been selected. Please add time')}</Typography>
+                <Typography color="warning">{TranslateText('No times have been selected')}</Typography>
             )}
             {allDays.map((day) => (
                 <div key={`Summary ${day}`}>
