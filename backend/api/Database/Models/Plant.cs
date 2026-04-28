d4832551ea45a060a107002fa9cbf990be4f0452
aestene <arnts@equinor.com>
2026-04-28 14:19:11 +0200

    Add optional Description property to Plant model

--- backend/api/Database/Models/Plant.cs
+++ backend/api/Database/Models/Plant.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 #pragma warning disable CS8618
@@ -20,5 +20,8 @@ namespace Api.Database.Models
         [Required]
         [MaxLength(200)]
         public string Name { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
     }
 }
