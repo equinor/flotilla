599ab8e07177d7024d20953556c7d43ed3930b0e
aestene <arnts@equinor.com>
2026-06-01 10:34:00 +0200

    Remove tag-zoom inspection endpoint

--- backend/api/Controllers/InspectionController.cs
+++ backend/api/Controllers/InspectionController.cs
@@ -17,45 +17,6 @@ namespace Api.Controllers
     ) : ControllerBase
     {
         /// <summary>
-        /// Updates the Flotilla metadata for an inspection tag
-        /// </summary>
-        /// <remarks>
-        /// <para> This query updates the Flotilla metadata for an inpection tag </para>
-        /// </remarks>
-        [HttpPost("{tagId}/tag-zoom")]
-        [Authorize(Roles = Role.Admin)]
-        [ProducesResponseType(typeof(TagInspectionMetadata), StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<TagInspectionMetadata>> Create(
-            [FromRoute] string tagId,
-            [FromBody] IsarZoomDescription zoom
-        )
-        {
-            tagId = Sanitize.SanitizeUserInput(tagId);
-
-            logger.LogInformation($"Updating zoom value for tag with ID {tagId}");
-
-            var newMetadata = new TagInspectionMetadata { TagId = tagId, ZoomDescription = zoom };
-
-            try
-            {
-                var metadata = await inspectionService.CreateOrUpdateTagInspectionMetadata(
-                    newMetadata
-                );
-
-                return metadata;
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, "Error while creating or updating inspection tag metadata");
-                throw;
-            }
-        }
-
-        /// <summary>
         /// Lookup the inspection image for task with specified isarInspectionId
         /// </summary>
         /// <remarks>
