39b996538751decb2d46e1e5459266b3481e553d
Nicholas Dalhaug <nicd@equinor.com>
2026-06-16 15:42:27 +0200

    Solve mistaken 2 hour offset locally

--- frontend/src/utils/StringFormatting.tsx
+++ frontend/src/utils/StringFormatting.tsx
@@ -1,23 +1,18 @@
 import { format } from 'date-fns'
 
-export const convertUTCDateToLocalDate = (date: Date): Date => {
-    // If lastChar is Z, typescript assumes the date to be UTC
-    const lastChar = date.toString().slice(-1)
-    if (lastChar === 'Z') return new Date(date)
-    // If not, typescript assumes the date to be local time
-    return new Date(new Date(date).getTime() - new Date(date).getTimezoneOffset() * 60 * 1000)
+export const convertUTCDateToLocalDate = (date: Date | string): Date => {
+    // If the input is actually a string (e.g. from JSON), ensure it is interpreted as UTC
+    if (typeof date === 'string') {
+        const dateStr = date as string
+        return new Date(dateStr.endsWith('Z') ? dateStr : dateStr + 'Z')
+    }
+    // If it is already a proper Date object, it already represents the correct point in time
+    return date
 }
 
-export const formatDateTime = (dateTime: Date, dateFormat: string): string =>
+export const formatDateTime = (dateTime: Date | string, dateFormat: string): string =>
     format(convertUTCDateToLocalDate(dateTime), dateFormat)
 
 export const capitalizeFirstLetter = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1)
-}
-
-export const formatDateString = (dateStr: Date | string) => {
-    let newStr = dateStr.toString()
-    newStr = newStr.slice(0, 19)
-    newStr = newStr.replaceAll('T', ' ')
-    return newStr
 }
